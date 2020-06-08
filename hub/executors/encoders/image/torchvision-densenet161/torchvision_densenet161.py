16e3d0fbea7a8e6f4a019f6672da864cd19a3eeb
saranshbh <saransh19bhargava@gmail.com>
2020-06-08 21:13:41 +0530

    Adding ResNet & DenseNet models to Image Executor

--- hub/executors/encoders/image/torchvision-densenet161/torchvision_densenet161.py
+++ hub/executors/encoders/image/torchvision-densenet161/torchvision_densenet161.py
@@ -4,6 +4,8 @@ class TorchVisionResNet18(ImageTorchEncoder):
 
     def __init__(self, *args, **kwargs):
         from torchvision.models import densenet161
-        super().__init__(*args, **kwargs)
-        self.model_name = densenet161(pretrained=True, progress=True)
+
+        self.model_name = densenet161(pretrained=True)
         self.pool_strategy = 'mean'
+
+        super().__init__(self.model_name, self.pool_strategy,*args, **kwargs)
--- hub/executors/encoders/image/torchvision-resnet18/torchvision_resnet18.py
+++ hub/executors/encoders/image/torchvision-resnet18/torchvision_resnet18.py
@@ -4,8 +4,9 @@ class TorchVisionResNet18(ImageTorchEncoder):
 
     def __init__(self, *args, **kwargs):
         from torchvision.models import resnet18
-        super().__init__(*args, **kwargs)
+
         self.model_name = resnet18(pretrained=True, progress=True)
         self.pool_strategy = 'max'
 
+        super().__init__(self.model_name, self.pool_strategy, *args, **kwargs)
 
