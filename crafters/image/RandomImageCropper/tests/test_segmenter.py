4fce26a10c81c95e4fe38ea8956165733ffa8a45
Han Xiao <han.xiao@jina.ai>
2020-08-19 18:46:44 +0200

    refactor(crafter): refactored sentenzier crafter

---
+++ crafters/image/RandomImageCropper/tests/test_segmenter.py
@@ -0,0 +1,16 @@
+from . import JinaImageTestCase
+from .. import RandomImageCropper
+
+
+class ImageSegmentTestCase(JinaImageTestCase):
+    def test_random_crop(self):
+        img_size = 217
+        img_array = self.create_random_img_array(img_size, img_size)
+        output_dim = 20
+        num_patches = 20
+        crafter = RandomImageCropper(output_dim, num_patches)
+        chunks = crafter.craft(img_array)
+        self.assertEqual(len(chunks), num_patches)
+        for chunk in chunks:
+            self.assertTrue(chunk['location'][0] <= (img_size - output_dim))
+            self.assertTrue(chunk['location'][1] <= (img_size - output_dim))
