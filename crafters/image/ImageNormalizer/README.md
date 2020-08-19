6ca8b903dc47d159288bf1d7cf261905c2b41721
Han Xiao <han.xiao@jina.ai>
2020-08-19 19:22:11 +0200

    refactor(crafter): refactored sentenzier crafter

---
+++ crafters/image/ImageNormalizer/README.md
@@ -0,0 +1,3 @@
+# ImageNormalizer
+
+it receives values of file names on the doc-level and returns image matrix on the chunk-level 
---
+++ crafters/image/ImageNormalizer/tests/test_imagenormalizer.py
@@ -0,0 +1,9 @@
+from .. import ImageNormalizer
+
+
+class TestClass:
+    def test_imagenormalizer(self):
+        """here is my test code
+
+        https://docs.pytest.org/en/stable/getting-started.html#create-your-first-test
+        """
