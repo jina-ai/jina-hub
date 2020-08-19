08ecaa01561fb7e85093f6b0138395ed502cfaac
Han Xiao <han.xiao@jina.ai>
2020-08-19 18:19:09 +0200

    refactor(crafter): refactored sentenzier crafter

---
+++ crafters/nlp/JiebaSegmenter/tests/test_jiebasegmenter.py
@@ -0,0 +1,9 @@
+from .. import JiebaSegmenter
+
+
+class TestClass:
+    def test_jieba_crafter(self):
+        jieba_crafter = JiebaSegmenter(mode='accurate')
+        text = '今天是个大晴天！安迪回来以后，我们准备去动物园。'
+        crafted_chunk_list = jieba_crafter.craft(text, 0)
+        assert len(crafted_chunk_list) == 14
