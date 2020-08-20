6df5750e1d5de1077f58ec3229120fb9b75d5930
Han Xiao <han.xiao@jina.ai>
2020-08-20 14:52:08 +0200

    fix: fix image crafter

--- crafters/image/FiveImageCropper/tests/test_segmenter.py
+++ crafters/image/FiveImageCropper/tests/test_segmenter.py
@@ -2,16 +2,20 @@ from . import JinaImageTestCase
 from .. import FiveImageCropper
 
 
-class ImageSegmentTestCase(JinaImageTestCase):
-    def test_five_image_crop(self):
-        img_size = 217
-        img_array = self.create_random_img_array(img_size, img_size)
-        output_dim = 20
-        crafter = FiveImageCropper(output_dim)
-        chunks = crafter.craft(img_array)
-        self.assertEqual(len(chunks), 5)
-        self.assertEqual(chunks[0]['location'], (0, 0))
-        self.assertEqual(chunks[1]['location'], (0, 197))
-        self.assertEqual(chunks[2]['location'], (197, 0))
-        self.assertEqual(chunks[3]['location'], (197, 197))
-        self.assertEqual(chunks[4]['location'], (98, 98))
+def create_random_img_array(img_height, img_width):
+    import numpy as np
+    return np.random.randint(0, 256, (img_height, img_width, 3))
+
+
+def test_five_image_crop(self):
+    img_size = 217
+    img_array = create_random_img_array(img_size, img_size)
+    output_dim = 20
+    crafter = FiveImageCropper(output_dim)
+    chunks = crafter.craft(img_array)
+    assert len(chunks) == 5
+    assert chunks[0]['location'] == (0, 0)
+    assert chunks[1]['location'] == (0, 197)
+    assert chunks[2]['location'] == (197, 0)
+    assert chunks[3]['location'] == (197, 197)
+    assert chunks[4]['location'] == (98, 98)
--- crafters/image/RandomImageCropper/tests/test_segmenter.py
+++ crafters/image/RandomImageCropper/tests/test_segmenter.py
@@ -10,7 +10,7 @@ class ImageSegmentTestCase(JinaImageTestCase):
         num_patches = 20
         crafter = RandomImageCropper(output_dim, num_patches)
         chunks = crafter.craft(img_array)
-        self.assertEqual(len(chunks), num_patches)
+        assert len(chunks) == num_patches
         for chunk in chunks:
             self.assertTrue(chunk['location'][0] <= (img_size - output_dim))
             self.assertTrue(chunk['location'][1] <= (img_size - output_dim))
