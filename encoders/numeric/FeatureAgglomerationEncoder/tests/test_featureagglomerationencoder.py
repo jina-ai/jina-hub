3f9eb3aa367f1833eb88c790235b96b1533cc057
JamesTang-jinaai <jingbang.tang@jina.ai>
2020-08-24 16:51:32 +0800

    refactor: migrate agglomeration encoder

---
+++ encoders/numeric/FeatureAgglomerationEncoder/tests/test_featureagglomerationencoder.py
@@ -0,0 +1,10 @@
+from .. import FeatureAgglomerationEncoder
+
+
+
+def test_featureagglomerationencoder():
+    """here is my test code
+
+    https://docs.pytest.org/en/stable/getting-started.html#create-your-first-test
+    """
+    raise NotImplementedError
