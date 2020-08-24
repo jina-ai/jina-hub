4acacff8c3614f5ac64ef7feadad8347925b42af
YueLiu-Jina <yue.liu@jina.ai>
2020-08-24 15:09:41 +0800

    refactor(encoder): refactor test for fastica

--- encoders/numeric/FastICAEncoder/tests/test_fasticaencoder.py
+++ encoders/numeric/FastICAEncoder/tests/test_fasticaencoder.py
@@ -1,17 +1,14 @@
-from .. import FastICAEncoder
-from encoders.numeric import TransformEncoder
+import numpy as np
 import os
 import pickle
-import numpy as np
 import shutil
 
 from jina.executors import BaseExecutor
-
+from .. import FastICAEncoder
 
 input_dim = 28
 target_output_dim = 2
 train_data = np.random.rand(2000, input_dim)
-
 
 def rm_files(tmp_files):
     for file in tmp_files:
@@ -23,79 +20,62 @@ def rm_files(tmp_files):
                     shutil.rmtree(file, ignore_errors=False, onerror=None)
 
 
-def get_encoder_TrainCase():
+def test_FastICATestCaseTrainCase():
+    requires_train_after_load = True
     encoder = FastICAEncoder(
         output_dim=target_output_dim, whiten=True, num_features=input_dim, max_iter=200)
     encoder.train(train_data)
-    return encoder
+    encoding_results(encoder)
+    save_and_load(encoder, requires_train_after_load)
+    save_and_load_config(encoder, requires_train_after_load)
 
-
-
-def test_encoding_results_TrainCase():
-    encoder = get_encoder_TrainCase()
-    test_data = np.random.rand(10, input_dim)
-    encoded_data = encoder.encode(test_data)
-    assert encoded_data.shape == (test_data.shape[0], target_output_dim)
-    assert type(encoded_data) is np.ndarray
-    rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
-
-def test_save_and_load_TrainCase():
-    encoder = get_encoder_TrainCase()
-    encoder.touch()
-    encoder.save()
-    assert os.path.exists(encoder.save_abspath)
-    rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
-
-
-def test_save_and_load_config_TrainCase():
-    encoder = get_encoder_TrainCase()
-    encoder.save_config()
-    assert os.path.exists(encoder.config_abspath)
-    encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-    encoder_loaded.train(np.random.rand(2000, input_dim))
-    test_data = np.random.rand(10, input_dim)
-    encoded_data_test = encoder_loaded.encode(test_data)
-    assert encoded_data_test.shape == (10, target_output_dim)
-    rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
-
-
-def get_encoder_LoadCase():
+def test_FastICATestCaseLoadCase():
+    requires_train_after_load = False
     encoder = FastICAEncoder(
         output_dim=target_output_dim, whiten=True, num_features=input_dim, max_iter=200)
+
     encoder.train(train_data)
     filename = 'ica_model.model'
     pickle.dump(encoder.model, open(filename, 'wb'))
-    return TransformEncoder(model_path=filename)
+    encoding_results(encoder)
+    save_and_load(encoder, requires_train_after_load)
+    save_and_load_config(encoder, requires_train_after_load)
 
 
-def test_encoding_results_LoadCase():
-    encoder = get_encoder_LoadCase()
+def encoding_results(encoder):
     test_data = np.random.rand(10, input_dim)
     encoded_data = encoder.encode(test_data)
     assert encoded_data.shape == (test_data.shape[0], target_output_dim)
     assert type(encoded_data) is np.ndarray
     rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
 
-def test_save_and_load_LoadCase():
-    encoder = get_encoder_LoadCase()
+
+def save_and_load(encoder, requires_train_after_load):
     test_data = np.random.rand(10, input_dim)
     encoded_data_control = encoder.encode(test_data)
     encoder.touch()
     encoder.save()
     assert os.path.exists(encoder.save_abspath)
     encoder_loaded = BaseExecutor.load(encoder.save_abspath)
-    encoded_data_test = encoder_loaded.encode(test_data)
-    # .all() to compare to vectors
-    assert encoded_data_test.all() == encoded_data_control.all()
+
+    if not requires_train_after_load:
+        # some models are not deterministic when training, so even with same training data, we cannot ensure
+        # same encoding results
+        encoded_data_test = encoder_loaded.encode(test_data)
+        np.testing.assert_array_equal(
+            encoded_data_test, encoded_data_control)
     rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
 
-def test_save_and_load_config_LoadCase():
-    encoder = get_encoder_LoadCase()
+
+def save_and_load_config(encoder, requires_train_after_load):
     encoder.save_config()
     assert os.path.exists(encoder.config_abspath)
     encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
+
+    if requires_train_after_load:
+        encoder_loaded.train(train_data)
+
     test_data = np.random.rand(10, input_dim)
     encoded_data_test = encoder_loaded.encode(test_data)
     assert encoded_data_test.shape == (10, target_output_dim)
     rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
-
