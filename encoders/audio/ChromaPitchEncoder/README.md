f2de5efe0366c1d2b429fffa4cb95c77b67a34b1
YueLiu-Jina <yue.liu@jina.ai>
2020-08-26 16:40:27 +0800

    refactor: wrap up

--- encoders/audio/ChromaPitchEncoder/README.md
+++ encoders/audio/ChromaPitchEncoder/README.md
@@ -1,3 +1,6 @@
 # ChromaPitchEncoder
 
-encodes an audio signal from a `Batch x Signal Length` ndarray into a `Batch x Concatenated Features` ndarray. 
+`ChromaPitchEncoder` is based on chroma spectrograms (chromagrams) which represent melodic/harmonic features.
+    
+`ChromaPitchEncoder` encodes an audio signal from a `Batch x Signal Length` ndarray into a
+    `Batch x Concatenated Features` ndarray.
--- encoders/audio/ChromaPitchEncoder/tests/test_chromapitchencoder.py
+++ encoders/audio/ChromaPitchEncoder/tests/test_chromapitchencoder.py
@@ -1,10 +1,12 @@
+import numpy as np
 from .. import ChromaPitchEncoder
 
 
-
-def test_chromapitchencoder():
-    """here is my test code
-
-    https://docs.pytest.org/en/stable/getting-started.html#create-your-first-test
-    """
-    raise NotImplementedError
+def test_chroma_encoder():
+    batch_size = 10
+    n_frames = 5
+    signal_length = 500 * n_frames
+    test_data = np.random.randn(batch_size, signal_length)
+    encoder = ChromaPitchEncoder()
+    encoded_data = encoder.encode(test_data)
+    assert encoded_data.shape == (batch_size, 12 * n_frames)
