acced7f80580be600ec2062ba55aa0be7baa5116
Han Xiao <han.xiao@jina.ai>
2020-08-26 19:22:14 +0200

    docs: update readme

--- chore_version.py
+++ chore_version.py
@@ -5,37 +5,44 @@ Example usage from TextPaddleHubEncoder executor directory:
 python ../../../manifest-version.py "manifest.yml"
 Commits the change in the branch and raises a PR for the executor.
 """
-import os
-import git
 import glob
+import os
 import subprocess
-import sys
+
+import git
 from ruamel.yaml import YAML
 
 repo_dir = os.getcwd()
 yaml = YAML()
 
-version_file = sys.argv[1]
-repo = git.Repo.init(repo_dir)
+version_file = 'manifest.yml'
+repo = git.Repo()
+previous_branch = repo.active_branch
 
-for fpath in glob.glob(f'**/{version_file}'):
-	dname = fpath.split("/")[-2]
-	with open(fpath) as fp:
-		info = yaml.load(fp)
-		old_ver = info['version']
-		new_ver = '.'.join(old_ver.split('.')[:-1] + [str(int(old_ver.split('.')[-1])+1)])
-		info['version'] = new_ver
-	with open(fpath, 'w') as fp:
-		yaml.dump(info, fp)
-	new_branch = repo.create_head(f'chore-version-{dname}')
-	new_branch.checkout()
-	index = repo.index
-	index.add(fpath)
-	index.commit('chore: bump version')
-	repo.git.push('--set-upstream', 'origin', new_branch)
-	title_string = f'bumping version for {dname}'
-	body_string = 'bumping version'
-	pr_command = f'gh pr create --title "{title_string}" --body "{body_string}"'
-	# Command with shell expansion
-	subprocess.call(pr_command, shell=True)
-	repo.git.checkout('master')
+for fpath in glob.glob(f'./**/{version_file}', recursive=True):
+    dname = fpath.split('/')[-2]
+    with open(fpath) as fp:
+        info = yaml.load(fp)
+        old_ver = info['version']
+        new_ver = '.'.join(old_ver.split('.')[:-1] + [str(int(old_ver.split('.')[-1]) + 1)])
+        info['version'] = new_ver
+    with open(fpath, 'w') as fp:
+        yaml.dump(info, fp)
+    try:
+        new_branch = repo.create_head(f'chore-{dname.lower()}-{new_ver.replace(".", "")}')
+        new_branch.checkout()
+        repo.git.add(update=True)
+        repo.index.commit(f'chore: bump version to {new_ver}')
+        origin = repo.remote(name='origin')
+        origin.push()
+
+        # make PR using `gh`
+        title_string = f'bumping version for {dname} to {new_ver}'
+        body_string = f'bumping version from {old_ver} to {new_ver}'
+        pr_command = f'gh pr create -f'
+        subprocess.call(pr_command, shell=True)
+        break
+    except:
+        raise
+    finally:
+        repo.git.checkout('master')
