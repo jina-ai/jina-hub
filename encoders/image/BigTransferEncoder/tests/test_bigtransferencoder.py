1197bf7581ac28bd4d8afdbd84fef0af5fc7c2d7
Nan Wang <nan.wang@conic.ai>
2020-08-28 00:13:20 +0800

    fix: minor fix

--- encoders/image/BigTransferEncoder/tests/test_bigtransferencoder.py
+++ encoders/image/BigTransferEncoder/tests/test_bigtransferencoder.py
@@ -19,7 +19,7 @@ def get_encoder():
     metas = get_default_metas()
     if 'JINA_TEST_GPU' in os.environ:
         metas['on_gpu'] = True
-    return BigTransferEncoder(model_path='BigTransferEncoder/pretrained', channel_axis=1, metas=metas)
+    return BigTransferEncoder(model_path='pretrained', channel_axis=1, metas=metas)
 
 
 def test_encoding_results():
