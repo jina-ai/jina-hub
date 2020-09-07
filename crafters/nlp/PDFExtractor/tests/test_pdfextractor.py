ed4b853038affa377eee2b2261d9ca193a169080
Susana Guzman <zuxi.w13@gmail.com>
2020-09-07 09:42:29 +0200

    refactor: move pdf cratfer to nlp folder

--- crafters/nlp/PDFExtractor/tests/test_pdfextractor.py
+++ crafters/nlp/PDFExtractor/tests/test_pdfextractor.py
@@ -1,9 +1,11 @@
 from .. import PDFTextExtractor
 
-def test_create_pdf():
+def create_pdf():
     from reportlab.pdfgen.canvas import Canvas
     canvas = Canvas("awesome_pdf_test.pdf")
     canvas.drawString(72, 72, "Cats rules")
     canvas.save()
 
 
+def test_read_pdf():
+    crafter = PDFTextExtractor()
