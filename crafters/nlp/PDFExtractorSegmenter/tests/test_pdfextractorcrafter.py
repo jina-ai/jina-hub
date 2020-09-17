87d2cb6dd70c5d786cc05b6ad2ef6b5c42d584fe
Susana Guzman <zuxi.w13@gmail.com>
2020-09-17 09:35:38 +0200

    test: extend tests

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdfextractorcrafter.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdfextractorcrafter.py
@@ -3,36 +3,24 @@ from PIL import Image
 import os
 
 expected_text = "A cat poem\nI love cats, I love every kind of cat,\nI just wanna hug all of them, but I can't," \
-                    "\nI'm thinking about cats again\nI think about how cute they are\nAnd their whiskers and their " \
-                    "nose\n "
+                "\nI'm thinking about cats again\nI think about how cute they are\nAnd their whiskers and their " \
+                "nose\n "
+
 
 def test_io_uri():
     crafter = PDFExtractorSegmenter()
     chunks = crafter.craft(uri='cats_are_awesome.pdf', buffer=None)
 
-    # Check image
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    img = Image.open(os.path.join(cur_dir, '../test_img.jpg'))
-    blob = chunks[0]['blob']
-    assert img.width == blob.width
-    assert img.height == blob.height
+    for i in range(len(chunks)):
+        print("list(chunks[", i, "])[0] ", list(chunks[i])[0])
+        if list(chunks[i])[0] == 'text':
+            # Check test
+            assert chunks[i]['text'] == expected_text
+        elif list(chunks[i])[0] == 'blob':
+            # Check image
+            cur_dir = os.path.dirname(os.path.abspath(__file__))
+            img = Image.open(os.path.join(cur_dir, '../test_img.jpg'))
+            blob = chunks[i]['blob']
+            assert img.width == blob.width
+            assert img.height == blob.height
 
-    # Check test
-    assert chunks[2]['text'] == expected_text
-
-
-def test_io_buffer():
-    with open('cats_are_awesome.pdf', 'rb') as pdf:
-        input_bytes = pdf.read()
-    crafter = PDFExtractorSegmenter()
-    chunks = crafter.craft(uri=None, buffer=input_bytes)
-
-    # Check image
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    img = Image.open(os.path.join(cur_dir, '../test_img.jpg'))
-    blob = chunks[0]['blob']
-    assert img.width == blob.width
-    assert img.height == blob.height
-
-    # Check test
-    assert chunks[2]['text'] == expected_text
