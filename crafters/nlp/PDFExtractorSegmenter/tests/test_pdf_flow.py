bdb2e6cf40fa0debf7e13defd87cf89723765992
Susana Guzman <zuxi.w13@gmail.com>
2020-09-18 15:18:59 +0200

    fix: remove print lines

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
@@ -6,16 +6,13 @@ from jina.flow import Flow
 import os
 
 
-def print_res(resp):
+def validate_text_fn(resp):
     expected_text = "A cat poem\nI love cats, I love every kind of cat,\nI just wanna hug all of them, but I can't," \
                     "\nI'm thinking about cats again\nI think about how cute they are\nAnd their whiskers and their " \
                     "nose\n"
 
     for d in resp.search.docs:
-        print(f'Ta-Dah🔮, here is what we found:')
-        print('👉%s' % d.chunks[0].text)
-
-    assert expected_text == d.chunks[0].text
+        assert expected_text == d.chunks[0].text
 
 
 
@@ -26,4 +23,4 @@ def test_pdf_flow():
     f = Flow().add(uses='PDFExtractorSegmenter')
 
     with f:
-        f.search([path], output_fn=print_res)
+        f.search([path], output_fn=validate_text_fn)
