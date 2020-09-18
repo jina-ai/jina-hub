0179c5b80d94c3107b85a199af31aee3494a39c0
Susana Guzman <zuxi.w13@gmail.com>
2020-09-18 18:48:03 +0200

    fix: add search generator

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
@@ -1,7 +1,5 @@
-from jina.flow import Flow
-import os
-
-
+from jina.proto import jina_pb2
+from jina.drivers.helper import array2pb
 from jina.flow import Flow
 import os
 
@@ -14,13 +12,17 @@ def validate_text_fn(resp):
     for d in resp.search.docs:
         assert expected_text == d.chunks[0].text
 
+def search_generator(path):
+    d = jina_pb2.Document()
+    d.uri = path
+    yield d
 
 
 def test_pdf_flow():
     cur_dir = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(cur_dir, 'cats_are_awesome_text.pdf')
+    path = os.path.join(cur_dir, 'cats_are_awesome.pdf')
 
     f = Flow().add(uses='PDFExtractorSegmenter')
 
     with f:
-        f.search([path], output_fn=validate_text_fn)
+        f.search(search_generator(path), output_fn=print)
