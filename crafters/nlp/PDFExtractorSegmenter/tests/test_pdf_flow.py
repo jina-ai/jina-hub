876aae5023f2eebe031714b7e51f905a65008eee
Susana Guzman <zuxi.w13@gmail.com>
2020-09-18 12:12:47 +0200

    fix: remove index

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
@@ -2,10 +2,21 @@ from jina.flow import Flow
 import os
 
 
+from jina.flow import Flow
+import os
+
+
 def print_res(resp):
+    expected_text = "A cat poem\nI love cats, I love every kind of cat,\nI just wanna hug all of them, but I can't," \
+                    "\nI'm thinking about cats again\nI think about how cute they are\nAnd their whiskers and their " \
+                    "nose\n"
+
     for d in resp.search.docs:
         print(f'Ta-Dah🔮, here is what we found:')
         print('👉%s' % d.chunks[0].text)
+
+    assert expected_text == d.chunks[0].text
+
 
 
 def test_pdf_flow():
@@ -13,9 +24,6 @@ def test_pdf_flow():
     path = os.path.join(cur_dir, 'cats_are_awesome_text.pdf')
 
     f = Flow().add(uses='PDFExtractorSegmenter')
-    with f:
-        f.index_files([path])
 
     with f:
         f.search([path], output_fn=print_res)
-
