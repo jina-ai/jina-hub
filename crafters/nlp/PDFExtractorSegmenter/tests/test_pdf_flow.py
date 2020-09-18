3c33a298de8add55d3a1436ae0ddf40d424cbfc5
Susana Guzman <zuxi.w13@gmail.com>
2020-09-18 10:18:01 +0200

    fix: print text results from pdf

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdf_flow.py
@@ -1,11 +1,21 @@
 from jina.flow import Flow
 import os
 
+
+def print_res(resp):
+    for d in resp.search.docs:
+        print(f'Ta-Dah🔮, here is what we found:')
+        print('👉%s' % d.chunks[0].text)
+
+
 def test_pdf_flow():
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(cur_dir, 'cats_are_awesome_text.pdf')
 
     f = Flow().add(uses='PDFExtractorSegmenter')
     with f:
-        f.index_files([path], output_fn=print)
+        f.index_files([path] )
+
+    with f:
+        f.search([path], output_fn=print_res)
 
