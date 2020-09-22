f6b18ab4256af05f6cc5321eb475d27e62cc1955
Susana Guzman <zuxi.w13@gmail.com>
2020-09-22 13:59:35 +0200

    test: assert size img validations

--- crafters/nlp/PDFExtractorSegmenter/tests/test_pdfextractorcrafter.py
+++ crafters/nlp/PDFExtractorSegmenter/tests/test_pdfextractorcrafter.py
@@ -1,3 +1,5 @@
+from pandas import np
+
 from .. import PDFExtractorSegmenter
 from PIL import Image
 import os
@@ -23,8 +25,12 @@ def test_io_uri_images_and_text():
         img = Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg'))
         blob = chunks[idx]['blob']
         assert blob.shape[1], blob.shape[0] == img.size
+        if idx is 0:
+            assert blob.shape == (660, 1024, 3)
+        if idx is 1:
+            assert blob.shape == (626, 1191, 3)
 
-    # Check test
+    # Check text
     assert chunks[2]['text'] == expected_text
 
 
@@ -49,6 +55,10 @@ def test_io_uri_img():
         img = Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg'))
         blob = chunks[idx]['blob']
         assert blob.shape[1], blob.shape[0] == img.size
+        if idx is 0:
+            assert blob.shape == (660, 1024, 3)
+        if idx is 1:
+            assert blob.shape == (626, 1191, 3)
 
 
 def test_io_buffer_images_and_text():
@@ -64,6 +74,10 @@ def test_io_buffer_images_and_text():
         img = Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg'))
         blob = chunks[idx]['blob']
         assert blob.shape[1], blob.shape[0] == img.size
+        if idx is 0:
+            assert blob.shape == (660, 1024, 3)
+        if idx is 1:
+            assert blob.shape == (626, 1191, 3)
 
     # Check test
     assert chunks[2]['text'] == expected_text
@@ -94,3 +108,7 @@ def test_io_buffer_img():
         img = Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg'))
         blob = chunks[idx]['blob']
         assert blob.shape[1], blob.shape[0] == img.size
+        if idx is 0:
+            assert blob.shape == (660, 1024, 3)
+        if idx is 1:
+            assert blob.shape == (626, 1191, 3)
