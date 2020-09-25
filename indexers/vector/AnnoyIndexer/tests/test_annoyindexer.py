a9e925e10e3c7709c88a6ce622884d47bf8234fb
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-09-25 19:39:29 +0200

    test: add tests for annoy and faiss

--- indexers/vector/AnnoyIndexer/tests/test_annoyindexer.py
+++ indexers/vector/AnnoyIndexer/tests/test_annoyindexer.py
@@ -36,11 +36,6 @@ def test_annoy_wrap_indexer():
     with BaseIndexer.load_config(os.path.join(cur_dir, 'yaml/annoy-wrap.yml')) as indexer:
         assert isinstance(indexer, AnnoyIndexer)
         idx, dist = indexer.query(query, top_k=4)
-        global retr_idx
-        if retr_idx is None:
-            retr_idx = idx
-        else:
-            np.testing.assert_almost_equal(retr_idx, idx)
         assert idx.shape == dist.shape
         assert idx.shape == (10, 4)
     rm_files([save_abspath, index_abspath])
@@ -67,11 +62,6 @@ def test_annoy_indexer():
     with BaseIndexer.load(save_abspath) as indexer:
         assert isinstance(indexer, AnnoyIndexer)
         idx, dist = indexer.query(query, top_k=4)
-        global retr_idx
-        if retr_idx is None:
-            retr_idx = idx
-        else:
-            np.testing.assert_almost_equal(retr_idx, idx)
         assert idx.shape == dist.shape
         assert idx.shape == (10, 4)
     rm_files([save_abspath, index_abspath])
@@ -93,3 +83,65 @@ def test_annoy_indexer_with_no_search_k():
         assert idx.shape == dist.shape
         assert idx.shape == (10, 0)
     rm_files([save_abspath, index_abspath])
+
+
+def test_annoy_indexer_known():
+    vectors = np.array([[1, 1, 1],
+                        [10, 10, 10],
+                        [100, 100, 100],
+                        [1000, 1000, 1000]])
+    keys = np.array([4, 5, 6, 7]).reshape(-1, 1)
+    with AnnoyIndexer(index_filename='annoy.test.gz') as indexer:
+        indexer.add(keys, vectors)
+        indexer.save()
+        assert os.path.exists(indexer.index_abspath)
+        index_abspath = indexer.index_abspath
+        save_abspath = indexer.save_abspath
+
+    queries = np.array([[1, 1, 1],
+                        [10, 10, 10],
+                        [100, 100, 100],
+                        [1000, 1000, 1000]])
+    with BaseIndexer.load(save_abspath) as indexer:
+        assert isinstance(indexer, AnnoyIndexer)
+        idx, dist = indexer.query(queries, top_k=2)
+        np.testing.assert_equal(idx, np.array([[4, 5], [5, 4], [6, 5], [7, 6]]))
+        assert idx.shape == dist.shape
+        assert idx.shape == (4, 2)
+        np.testing.assert_equal(indexer.query_by_id([7, 4]), vectors[[3, 0]])
+
+    rm_files([index_abspath, save_abspath])
+
+
+def test_annoy_indexer_known_big():
+    """Let's try to have some real test. We will have an index with 10k vectors of random values between 5 and 10.
+     We will change tweak some specific vectors that we expect to be retrieved at query time. We will tweak vector
+     at index [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000], this will also be the query vectors.
+     Then the keys will be assigned shifted to test the proper usage of `int2ext_id` and `ext2int_id`
+    """
+    vectors = np.random.uniform(low=5.0, high=10.0, size=(10000, 1024))
+
+    queries = np.empty((10, 1024))
+    for idx in range(0, 10000, 1000):
+        array = idx*np.ones((1, 1024))
+        queries[int(idx/1000)] = array
+        vectors[idx] = array
+
+    keys = np.arange(10000, 20000).reshape(-1, 1)
+
+    with AnnoyIndexer(index_filename='annoy.test.gz') as indexer:
+        indexer.add(keys, vectors)
+        indexer.save()
+        assert os.path.exists(indexer.index_abspath)
+        index_abspath = indexer.index_abspath
+        save_abspath = indexer.save_abspath
+
+    with BaseIndexer.load(save_abspath) as indexer:
+        assert isinstance(indexer, AnnoyIndexer)
+        idx, dist = indexer.query(queries, top_k=1)
+        np.testing.assert_equal(idx, np.array([[10000], [11000], [12000], [13000], [14000], [15000], [16000], [17000], [18000], [19000]]))
+        assert idx.shape == dist.shape
+        assert idx.shape == (10, 1)
+        np.testing.assert_equal(indexer.query_by_id([10000, 15000]), vectors[[0, 5000]])
+
+    rm_files([index_abspath, save_abspath])
