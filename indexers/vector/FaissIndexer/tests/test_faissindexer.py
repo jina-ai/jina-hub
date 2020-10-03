42efbc40fa7a1e79dff31cb67ea241b3903ced7d
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-10-03 19:43:31 +0200

    test: add tmpdir

--- indexers/vector/FaissIndexer/tests/test_faissindexer.py
+++ indexers/vector/FaissIndexer/tests/test_faissindexer.py
@@ -1,9 +1,9 @@
 import gzip
 import os
-import shutil
 
 import numpy as np
 from jina.executors.indexers import BaseIndexer
+from jina.executors.metas import get_default_metas
 
 from .. import FaissIndexer
 
@@ -15,27 +15,19 @@ vec = np.array(np.random.random([10, 10]), dtype=np.float32)
 query = np.array(np.random.random([10, 10]), dtype=np.float32)
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
-
-def rm_files(file_paths):
-    for file_path in file_paths:
-        if os.path.exists(file_path):
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path, ignore_errors=False, onerror=None)
-
-
-def test_faiss_indexer():
-    train_filepath = os.path.join(cur_dir, 'train.tgz')
+def test_faiss_indexer(tmpdir):
+    os.environ['TEST_WORKSPACE'] = str(tmpdir)
+    metas = get_default_metas()
+    metas['workspace'] = os.environ['TEST_WORKSPACE']
+    train_filepath = os.path.join(tmpdir, 'train.tgz')
     train_data = np.array(np.random.random([1024, 10]), dtype=np.float32)
     with gzip.open(train_filepath, 'wb', compresslevel=1) as f:
         f.write(train_data.tobytes())
 
-    with FaissIndexer(index_filename='faiss.test.gz', index_key='IVF10,PQ2', train_filepath=train_filepath) as indexer:
+    with FaissIndexer(index_filename='faiss.test.gz', index_key='IVF10,PQ2', train_filepath=train_filepath, metas=metas) as indexer:
         indexer.add(vec_idx, vec)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
-        index_abspath = indexer.index_abspath
         save_abspath = indexer.save_abspath
 
     with BaseIndexer.load(save_abspath) as indexer:
@@ -43,27 +35,28 @@ def test_faiss_indexer():
         idx, dist = indexer.query(query, top_k=4)
         assert idx.shape == dist.shape
         assert idx.shape == (10, 4)
+    del os.environ['TEST_WORKSPACE']
 
-    rm_files([index_abspath, save_abspath, train_filepath])
 
-
-def test_faiss_indexer_known():
+def test_faiss_indexer_known(tmpdir):
+    os.environ['TEST_WORKSPACE'] = str(tmpdir)
+    metas = get_default_metas()
+    metas['workspace'] = os.environ['TEST_WORKSPACE']
     vectors = np.array([[1, 1, 1],
                         [10, 10, 10],
                         [100, 100, 100],
                         [1000, 1000, 1000]], dtype=np.float32)
     keys = np.array([4, 5, 6, 7]).reshape(-1, 1)
 
-    train_filepath = os.path.join(cur_dir, 'train.tgz')
+    train_filepath = os.path.join(tmpdir, 'train.tgz')
     train_data = vectors
     with gzip.open(train_filepath, 'wb', compresslevel=1) as f:
         f.write(train_data.tobytes())
 
-    with FaissIndexer(index_filename='faiss.test.gz', index_key='Flat', train_filepath=train_filepath) as indexer:
+    with FaissIndexer(index_filename='faiss.test.gz', index_key='Flat', train_filepath=train_filepath, metas=metas) as indexer:
         indexer.add(keys, vectors)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
-        index_abspath = indexer.index_abspath
         save_abspath = indexer.save_abspath
 
     queries = np.array([[1, 1, 1],
@@ -77,16 +70,18 @@ def test_faiss_indexer_known():
         assert idx.shape == dist.shape
         assert idx.shape == (4, 2)
         np.testing.assert_equal(indexer.query_by_id([7, 4]), vectors[[3, 0]])
+    del os.environ['TEST_WORKSPACE']
 
-    rm_files([index_abspath, save_abspath, train_filepath])
 
-
-def test_faiss_indexer_known_big():
+def test_faiss_indexer_known_big(tmpdir):
     """Let's try to have some real test. We will have an index with 10k vectors of random values between 5 and 10.
      We will change tweak some specific vectors that we expect to be retrieved at query time. We will tweak vector
      at index [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000], this will also be the query vectors.
      Then the keys will be assigned shifted to test the proper usage of `int2ext_id` and `ext2int_id`
     """
+    os.environ['TEST_WORKSPACE'] = str(tmpdir)
+    metas = get_default_metas()
+    metas['workspace'] = os.environ['TEST_WORKSPACE']
     vectors = np.random.uniform(low=5.0, high=10.0, size=(10000, 1024)).astype('float32')
 
     queries = np.empty((10, 1024), dtype=np.float32)
@@ -95,18 +90,17 @@ def test_faiss_indexer_known_big():
         queries[int(idx/1000)] = array
         vectors[idx] = array
 
-    train_filepath = os.path.join(cur_dir, 'train.tgz')
+    train_filepath = os.path.join(tmpdir, 'train.tgz')
     train_data = vectors
     with gzip.open(train_filepath, 'wb', compresslevel=1) as f:
         f.write(train_data.tobytes())
 
     keys = np.arange(10000, 20000).reshape(-1, 1)
 
-    with FaissIndexer(index_filename='faiss.test.gz', index_key='Flat', train_filepath=train_filepath) as indexer:
+    with FaissIndexer(index_filename='faiss.test.gz', index_key='Flat', train_filepath=train_filepath, metas=metas) as indexer:
         indexer.add(keys, vectors)
         indexer.save()
         assert os.path.exists(indexer.index_abspath)
-        index_abspath = indexer.index_abspath
         save_abspath = indexer.save_abspath
 
     with BaseIndexer.load(save_abspath) as indexer:
@@ -116,5 +110,4 @@ def test_faiss_indexer_known_big():
         assert idx.shape == dist.shape
         assert idx.shape == (10, 1)
         np.testing.assert_equal(indexer.query_by_id([10000, 15000]), vectors[[0, 5000]])
-
-    rm_files([index_abspath, save_abspath, train_filepath])
+    del os.environ['TEST_WORKSPACE']
