32bd2909b1680f99b02a49e7710ae50807fc3b4d
Joan Fontanals Martinez <joan.martinez@jina.ai>
2020-10-13 14:13:27 +0200

    fix: raise PretrainedModelFileDoesNotExist when model_path not existing

--- encoders/image/BigTransferEncoder/tests/test_bigtransferencoder.py
+++ encoders/image/BigTransferEncoder/tests/test_bigtransferencoder.py
@@ -1,53 +1,39 @@
+import pytest
 import os
-import shutil
 import numpy as np
+
 from .. import BigTransferEncoder
 from jina.executors.metas import get_default_metas
 from jina.executors import BaseExecutor
+from jina.excepts import PretrainedModelFileDoesNotExist
 
 
-def rm_files(file_paths):
-    for file_path in file_paths:
-        if os.path.exists(file_path):
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path, ignore_errors=False, onerror=None)
-
-
-def get_encoder():
+def get_encoder(model_path_tmp_dir):
     metas = get_default_metas()
     if 'JINA_TEST_GPU' in os.environ:
         metas['on_gpu'] = True
+        metas['workspace'] = model_path_tmp_dir
     return BigTransferEncoder(model_path='pretrained', channel_axis=1, metas=metas)
 
 
+def test_encoding_results(tmpdir):
+    input_dim = 48
+    output_dim = 2048
+    encoder = get_encoder(str(tmpdir))
+    test_data = np.random.rand(2, 3, input_dim, input_dim)
+    encoded_data = encoder.encode(test_data)
+    assert encoded_data.shape == (2, output_dim)
 
 
-# def test_encoding_results():
-#     input_dim = 48
-#     output_dim = 2048
-#     encoder = get_encoder()
-#     test_data = np.random.rand(2, 3, input_dim, input_dim)
-#     encoded_data = encoder.encode(test_data)
-#     assert encoded_data.shape == (2, output_dim)
-#     rm_files([encoder.save_abspath, encoder.config_abspath])
-#
-#
-# def test_save_and_load():
-#     encoder = get_encoder()
-#     encoder.touch()
-#     encoder.save()
-#     assert os.path.exists(encoder.save_abspath)
-#     encoder_loaded = BaseExecutor.load(encoder.save_abspath)
-#     assert encoder_loaded.channel_axis == encoder.channel_axis
-#     rm_files([encoder.save_abspath, encoder.config_abspath])
+def test_save_and_load(tmpdir):
+    encoder = get_encoder(str(tmpdir))
+    encoder.touch()
+    encoder.save()
+    assert os.path.exists(encoder.save_abspath)
+    encoder_loaded = BaseExecutor.load(encoder.save_abspath)
+    assert encoder_loaded.channel_axis == encoder.channel_axis
 
 
-def test_save_and_load_config():
-    encoder = get_encoder()
-    encoder.save_config()
-    assert os.path.exists(encoder.config_abspath)
-    encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-    assert encoder_loaded.model_path == encoder.model_path
-    # rm_files([encoder.save_abspath, encoder.config_abspath])
+def test_raise_exception():
+    with pytest.raises(PretrainedModelFileDoesNotExist):
+        assert BigTransferEncoder()
