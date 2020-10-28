bea6dcd8342c384efa5293d6f6bccf834b675eba
bwanglzu <kingbolanda@live.com>
2020-10-28 01:25:34 +0100

    feat: add ndcg evaluator

--- evaluators/rank/NdcgEvaluator/__init__.py
+++ evaluators/rank/NdcgEvaluator/__init__.py
@@ -12,28 +12,41 @@ class NDCGEvaluator(BaseRankingEvaluator):
     def metric(self):
         return f'nDCG@{self.eval_at}'
 
-    def evaluate(self, actual: Sequence[Any], desired: Sequence[Any], *args, **kwargs) -> float:
+    def evaluate(
+            self,
+            actual: Sequence[Any],
+            desired: Sequence[Any],
+            use_traditional_formula=False,
+            *args, **kwargs
+    ) -> float:
         """"
         :param actual: the scores predicted by the search system.
         :param desired: the expected score given by user as groundtruth.
+        :param use_traditional_formula: if use traditional formula.
+            The new formula places stronger emphasis on retrieving relevant documents.
+            For detailed information, please check https://en.wikipedia.org/wiki/Discounted_cumulative_gain
         :return the evaluation metric value for the request document.
         """
         actual_at_k = actual[:self.eval_at]
         desired_at_k = desired[:self.eval_at]
         if len(actual) < 2:
             raise ValueError(f'Expecting gains with minimal length of 2, {len(actual)} received.')
-        dcg = self._compute_dcg(gains=actual_at_k)
-        idcg = self._compute_idcg(gains=desired_at_k)
+        dcg  = self._compute_dcg(gains=actual_at_k, use_traditional_formula=use_traditional_formula)
+        idcg = self._compute_idcg(gains=desired_at_k, use_traditional_formula=use_traditional_formula)
         return 0.0 if idcg == 0.0 else dcg/idcg
 
-    def _compute_dcg(self, gains):
+    def _compute_dcg(self, gains, use_traditional_formula):
         """Compute discounted cumulative gain."""
         ret = 0.0
-        for score, position in zip(gains[1:], range(2, len(gains) + 1)):
-            ret += score/log(position, 2)
-        return gains[0] + ret
+        if use_traditional_formula:
+            for score, position in zip(gains[1:], range(2, len(gains) + 1)):
+                ret += score/log(position, 2)
+            return gains[0] + ret
+        for score, position in zip(gains, range(1, len(gains) + 1)):
+            ret += (pow(2, score) - 1)/log(position + 1, 2)
+        return ret
 
-    def _compute_idcg(self, gains):
+    def _compute_idcg(self, gains, use_traditional_formula):
         """Compute ideal discounted cumulative gain."""
         sorted_gains = sorted(gains, reverse=True)
-        return self._compute_dcg(sorted_gains)
+        return self._compute_dcg(sorted_gains, use_traditional_formula)
--- evaluators/rank/NdcgEvaluator/tests/test_ndcg_evaluator.py
+++ evaluators/rank/NdcgEvaluator/tests/test_ndcg_evaluator.py
@@ -10,15 +10,22 @@ def evaluator():
 def desired():
     return [.8, .4, .1, 0]
 
-@pytest.mark.parametrize('actual, expected',
+@pytest.mark.parametrize('actual, use_traditional_formula, expected',
     [
-        ([.4, .1, .8], 0.795),
-        ([.0, .1, .4], 0.279),
-        ([.4, .1, .0], 0.396),
+        ([.4, .1, .8], True, 0.795),
+        ([.0, .1, .4], True, 0.279),
+        ([.4, .1, .0], True, 0.396),
+        ([.4, .1, .8], False, 0.795),
+        ([.0, .1, .4], False, 0.210),
+        ([.4, .1, .0], False, 0.396),
+        ([.0, .0, .0], True, 0.0), # no match
+        ([.0, .0, .0], False, 0.0),
+        ([.8, .4, .1], True, 1.0), # all match
+        ([.8, .4, .1], False, 1.0),
     ]
 )
-def test_encode_success(evaluator, actual, desired, expected):
-    assert evaluator.evaluate(actual=actual, desired=desired) == pytest.approx(expected, 0.1)
+def test_encode(evaluator, actual, desired, use_traditional_formula, expected):
+    assert evaluator.evaluate(actual=actual, desired=desired, use_traditional_formula=use_traditional_formula) == pytest.approx(expected, 0.1)
 
 def test_encode_fail(evaluator, desired):
     with pytest.raises(ValueError):
