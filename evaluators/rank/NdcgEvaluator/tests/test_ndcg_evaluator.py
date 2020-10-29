a161ebcd04f00919682c481cf8abd522401f77c2
bwanglzu <kingbolanda@live.com>
2020-10-30 00:07:24 +0100

    feat: add more tests

--- evaluators/rank/NdcgEvaluator/tests/test_ndcg_evaluator.py
+++ evaluators/rank/NdcgEvaluator/tests/test_ndcg_evaluator.py
@@ -6,23 +6,33 @@ from .. import NDCGEvaluator
 def evaluator():
     return NDCGEvaluator(eval_at=3)
 
-@pytest.mark.parametrize('actual, use_traditional_formula, expected',
+@pytest.mark.parametrize('actual, evaluator, use_traditional_formula, expected',
     [
-        ([.4, .1, .8], True, 0.795),
-        ([.0, .1, .4], True, 0.279),
-        ([.4, .1, .0], True, 0.396),
-        ([.4, .1, .8], False, 0.795),
-        ([.0, .1, .4], False, 0.210),
-        ([.4, .1, .0], False, 0.396),
-        ([.0, .0, .0], True, 0.0), # no match
-        ([.0, .0, .0], False, 0.0),
-        ([.8, .4, .1], True, 1.0), # all match
-        ([.8, .4, .1], False, 1.0),
+        ([.4, .1, .8], NDCGEvaluator(eval_at=3), True, 0.795),
+        ([.0, .1, .4], NDCGEvaluator(eval_at=3), True, 0.279),
+        ([.4, .1, .0], NDCGEvaluator(eval_at=3), True, 0.396),
+        ([.4, .1, .8], NDCGEvaluator(eval_at=3), False, 0.751),
+        ([.0, .1, .4], NDCGEvaluator(eval_at=3), False, 0.209),
+        ([.4, .1, .0], NDCGEvaluator(eval_at=3),  False, 0.373),
+        ([.0, .0, .0], NDCGEvaluator(eval_at=3), True, 0.0), # no match
+        ([.0, .0, .0], NDCGEvaluator(eval_at=3), False, 0.0),
+        ([.8, .4, .1], NDCGEvaluator(eval_at=3), True, 1.0), # all match
+        ([.8, .4, .1], NDCGEvaluator(eval_at=3), False, 1.0),
+        ([.4, .1, .8], NDCGEvaluator(eval_at=2), False, 0.387),
+        ([.4, .1, .8], NDCGEvaluator(eval_at=2), True, 0.417),
+        ([.4, .1, .8], NDCGEvaluator(eval_at=5), True, 0.795),
     ]
 )
-def test_encode(evaluator, actual, use_traditional_formula, expected):
-    assert evaluator.evaluate(actual=actual, desired=[.8, .4, .1, 0], use_traditional_formula=use_traditional_formula) == pytest.approx(expected, 0.1)
+def test_encode(actual, evaluator, use_traditional_formula, expected):
+    assert evaluator.evaluate(
+        actual=actual,
+        desired=[.8, .4, .1, 0],
+        use_traditional_formula=use_traditional_formula
+    ) == pytest.approx(expected, 0.01)
 
-def test_encode_fail(evaluator):
+@pytest.mark.parametrize('actual', [
+    ([]), ([0.8])
+])
+def test_encode_fail(evaluator, actual):
     with pytest.raises(ValueError):
-        evaluator.evaluate(actual=[0.8], desired=[.8, .4, .1, 0])
+        evaluator.evaluate(actual=actual, desired=[.8, .4, .1, 0])
