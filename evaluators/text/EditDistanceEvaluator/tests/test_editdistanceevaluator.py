eaa5eec4a082fcf7c924ed8cf5697590241548dd
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-10-31 17:16:56 +0530

    test: added test case with punctuation

--- evaluators/text/EditDistanceEvaluator/tests/test_editdistanceevaluator.py
+++ evaluators/text/EditDistanceEvaluator/tests/test_editdistanceevaluator.py
@@ -8,6 +8,8 @@ from .. import EditDistanceEvaluator
         ('hello', 'hello', 0.0),
         ('Hello', 'hello', 1.0),
         ('1Hello1', 'hello', 3.0),
+        ('hello!@#', 'hello', 3.0),
+        ('hell!o', 'hello', 1.0),
         ('', '', 0.0),
         ('hello', 'helo', 1.0),
         ('hello', '', 5.0),
