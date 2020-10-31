1bfe142a623e9d376c30676e1826300561f6de77
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-10-31 16:25:50 +0530

    fix: name changes

--- evaluators/text/RougeEvaluator/__init__.py
+++ evaluators/text/RougeEvaluator/__init__.py
@@ -9,19 +9,23 @@ class RougeEvaluator(BaseTextEvaluator):
     :class:`RougeEvaluator` Evaluate Rouge score between acutal and ground truth.
     """
 
-    def __init__(self, metrics: str='rouge-1', stats: str='r', *args, **kwargs):
-        """metrics: can be rouge-1, rouge-2 or rouge-l
-        stats: can be r, p or f
+    def __init__(self, metric: str='rouge-1', stat: str='r', *args, **kwargs):
+        """metric: can be rouge-1, rouge-2 or rouge-l
+        stat: can be r, p or f
         """
         super().__init__(*args, **kwargs)
-        self.metrics = metrics
-        self.stats = stats
-        self.rouge = Rouge(metrics=[metrics], stats=[stats])
+        self.metric_ = metric
+        self.stat = stat
+        self.rouge = Rouge(metrics=[metric], stats=[stat])
+
+    @property
+    def metric(self):
+        return f'{self.metric_.upper()}'
 
     def _get_score(self, actual_, desired_):
         if (not len(actual_)) or (not len(desired_)):
             return 0.0
-        return self.rouge.get_scores(actual_, desired_)[0][self.metrics][self.stats]
+        return self.rouge.get_scores(actual_, desired_)[0][self.metric_][self.stat]
 
     def evaluate(self, actual: Sequence[str], desired: Sequence[str]) -> float:
         return sum(self._get_score(actual_, desired_) for actual_, desired_ in zip(actual, desired))
