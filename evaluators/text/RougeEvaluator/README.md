66c1d6b33b364f6f290cebdc7dbd40c47176c49c
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-11-02 14:16:49 +0530

    feat: add rouge evaluator
    * feat: add rouge evaluator
    
    * fix: name changes
    
    * fix: added post init and changed seq to str
    
    * test: with recall and f1

---
+++ evaluators/text/RougeEvaluator/README.md
@@ -0,0 +1,3 @@
+# RougeEvaluator
+
+Evaluate ROUGE score between actual and ground truth texts.
---
+++ evaluators/text/RougeEvaluator/__init__.py
@@ -0,0 +1,28 @@
+from jina.executors.evaluators.text import BaseTextEvaluator
+
+class RougeEvaluator(BaseTextEvaluator):
+    """
+    :class:`RougeEvaluator` Evaluate Rouge score between acutal and ground truth.
+    """
+
+    def __init__(self, metric: str='rouge-1', stat: str='r', *args, **kwargs):
+        """metric: can be rouge-1, rouge-2 or rouge-l
+        stat: can be r for recall, p for precision and f for f1
+        """
+        super().__init__(*args, **kwargs)
+        self._metric = metric.lower()
+        self.stat = stat.lower()
+
+    def post_init(self):
+        super().post_init()
+        from rouge import Rouge
+        self.rouge = Rouge(metrics=[self._metric], stats=[self.stat])
+
+    @property
+    def metric(self):
+        return f'{self._metric.upper()}'
+
+    def evaluate(self, actual: str, desired: str) -> float:
+        if (not len(actual)) or (not len(desired)):
+            return 0.0
+        return float(self.rouge.get_scores(actual, desired)[0][self._metric][self.stat])
