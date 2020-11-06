248055ac7334a0e07357a8548267e10e459272df
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2020-11-06 11:44:22 +0530

    fix: rename test file

--- evaluators/rank/MeanReciprocalRankEvaluator/tests/test_meanreciprocalrankevaluator.py
+++ evaluators/rank/MeanReciprocalRankEvaluator/tests/test_meanreciprocalrankevaluator.py
@@ -14,6 +14,6 @@ from .. import MeanReciprocalRankEvaluator
     ([[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4], [2, 1, 3, 4]], (1+0.5)/2)
 ]
 )
-def test_reciprocalrankevaluator(actual, desired, score):
+def test_meanreciprocalrankevaluator(actual, desired, score):
     evaluator = MeanReciprocalRankEvaluator(eval_at=3)
     assert evaluator.evaluate(actual, desired) == score
