42e14e408b9240f302943a99564399c5370e3361
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2020-11-08 18:03:34 +0100

    feat(evaluator): add average precision (#474)
    * feat(evaluator): add average precision
    
    * fix(evaluator): adapt config and readme to real class name
    
    * fix(evaluator): hide eval_at parameter as suggested by @nan-wang
    
    eval_at is not used in AveragePrecision.
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>
    
    Co-authored-by: Nan Wang <nan.wang@jina.ai>

---
+++ evaluators/rank/AveragePrecision/README.md
@@ -0,0 +1,3 @@
+# AveragePrecisionEvaluator
+Calculate Average Precision between two rankings as defined for [information retrieval](https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision).
+
---
+++ evaluators/rank/AveragePrecision/tests/test_averageprecisionevaluator.py
@@ -0,0 +1,39 @@
+from .. import AveragePrecisionEvaluator
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize(
+    'matches_ids, desired_ids, expected',
+    [
+        ([0, 1, 2, 3], [0, 1, 2, 3], 1.0),
+        ([0, 1, 4, 5], [0, 1, 2, 3], 0.5),
+        ([4, 5, 6, 7], [0, 1, 2, 3], 0.0),
+        ([0, 1, 1, 1], [0, 1, 2, 3], 0.5),
+        ([0, 1], [0, 1, 2, 3], 0.5),
+        ([0, 1, 4, 2], [0, 1, 2, 3], 0.5),
+        ([0, 1, 3], [0, 1, 2, 3], 0.677083),
+        ([0, 1, 3, 2], [0, 1, 2, 3], 0.677083),
+    ]
+)
+def test_average_precision_evaluator(matches_ids, desired_ids, expected):
+
+    evaluator = AveragePrecisionEvaluator()
+    output = evaluator.evaluate(actual=matches_ids, desired=desired_ids)
+    np.testing.assert_array_almost_equal(output, expected)
+
+
+def test_precision_evaluator_no_groundtruth():
+    matches_ids = [0, 1, 2, 3, 4]
+    desired_ids = []
+
+    evaluator = AveragePrecisionEvaluator()
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == 0.0
+
+
+def test_precision_evaluator_no_actuals():
+    matches_ids = []
+    desired_ids = [1, 2]
+
+    evaluator = AveragePrecisionEvaluator()
+    assert evaluator.evaluate(actual=matches_ids, desired=desired_ids) == 0.0
