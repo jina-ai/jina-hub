72c77d7f2b3b89c380864e55986fb61132548ed2
Siyuan Shi <shimuyi0428@gmail.com>
2020-11-09 19:19:37 +0100

    feat: add Jaccard similarity evaluator

---
+++ evaluators/text/JaccardSimilarityEvaluator/tests/test_jaccardsimilarityevaluator.py
@@ -0,0 +1,19 @@
+import pytest
+
+from .. import JaccardSimilarityEvaluator
+
+
+@pytest.mark.parametrize('actual, desired, distance',
+    [
+        ('hello world', 'hello world', 1.0),
+        ('HELLO WORLD', 'hello world', 1.0),
+        ('hello world', 'hello      ', 0.5),
+        ('hello      ', 'hello world', 0.5),
+        ('gdkkm      ', 'hello      ', 0.0),
+        ('hey yup man', 'whats up man', 0.2),
+        ('', '', 0.0),
+    ]
+)
+def test_jaccardsimilarityevaluator(actual, desired, distance):
+    evaluator = JaccardSimilarityEvaluator()
+    assert evaluator.evaluate(actual, desired) == pytest.approx(distance)
