bfe8ba8fb7cbed7b6a262f236962653b84a64bb8
Florian Hönicke <hoenicke.florian@gmail.com>
2020-11-09 14:41:50 +0100

    feat(ranker): move deprecation warning to constructor

--- rankers/MinRanker/__init__.py
+++ rankers/MinRanker/__init__.py
@@ -1,16 +1,19 @@
 from jina.executors.rankers import Chunk2DocRanker
-import warnings
-
-warnings.warn("MinRanker is deprecated. Please use SimpleAggregateRanker instead", DeprecationWarning, stacklevel=2)
 
 
 class MinRanker(Chunk2DocRanker):
     """
-    :class:`MinRanker` calculates the score of the matched doc form the matched chunks. For each matched doc, the score
+    :class:`MinRanker` calculates the score of the matched doc from the matched chunks. For each matched doc, the score
         is `1 / (1 + s)`, where `s` is the minimal score from all the matched chunks belonging to this doc.
 
     .. warning:: Here we suppose that the smaller chunk score means the more similar.
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        import warnings
+        warnings.warn("MinRanker is deprecated. Please use SimpleAggregateRanker instead", DeprecationWarning,
+                      stacklevel=2)
 
     def _get_score(self, match_idx, query_chunk_meta, match_chunk_meta, *args, **kwargs):
         return self.get_doc_id(match_idx), 1. / (1. + match_idx[self.COL_SCORE].min())
