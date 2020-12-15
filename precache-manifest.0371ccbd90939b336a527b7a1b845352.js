607796278d0c90a84f0c3c4dd39aa6b0d4efce5f
imsergiy <sergiychemodanov@gmail.com>
2020-12-15 05:32:58 +0000

    Deploying to gh-pages from  @ c153e4b56ff07e3afb3565c734a2a7e82c44b274 🚀

---
+++ precache-manifest.0371ccbd90939b336a527b7a1b845352.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532ccd60662a54d22659f62d239e0ef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d2315f360b0ed67e852",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e4bbd1063deddefa778a",
+    "url": "/static/css/main.85d6682a.chunk.css"
+  },
+  {
+    "revision": "2d2315f360b0ed67e852",
+    "url": "/static/js/2.b1545613.chunk.js"
+  },
+  {
+    "revision": "7a6f34fe2c407257077a6a6e69aa23ba",
+    "url": "/static/js/2.b1545613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bbd1063deddefa778a",
+    "url": "/static/js/main.d5492c65.chunk.js"
+  },
+  {
+    "revision": "9ce47470022abec0dbe6",
+    "url": "/static/js/runtime-main.609ee7a5.js"
+  }
+]);
