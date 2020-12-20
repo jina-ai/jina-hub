63ae5a5f61566a53c869bbf25a832ffbd8bbb2f7
Tadej Svetina <tadej.svetina@redstone.vc>
2020-12-20 12:55:32 +0100

    perf(encoder): improve torch transformer encoder speed

--- encoders/nlp/TransformerTorchEncoder/__init__.py
+++ encoders/nlp/TransformerTorchEncoder/__init__.py
@@ -8,59 +8,40 @@ import numpy as np
 from jina.executors.decorators import batching, as_ndarray
 from jina.executors.devices import TorchDevice
 from jina.executors.encoders import BaseEncoder
-from jina.executors.encoders.helper import reduce_mean, reduce_max, reduce_min, reduce_cls
 from jina.helper import cached_property
 from jina.logging import default_logger
 
 
-def auto_reduce(model_outputs: 'np.ndarray', mask_2d: 'np.ndarray', model_name: str) -> 'np.ndarray':
-    """
-    Automatically creates a sentence embedding from its token embeddings.
-        * For BERT-like models (BERT, RoBERTa, DistillBERT, Electra ...) uses embedding of first token
-        * For XLM and XLNet models uses embedding of last token
-        * Assumes that other models are language-model like and uses embedding of last token
-    """
-    if 'bert' in model_name or 'electra' in model_name:
-        return reduce_cls(model_outputs, mask_2d)
-    if 'xlnet' in model_name:
-        return reduce_cls(model_outputs, mask_2d, cls_pos='tail')
-    default_logger.warning('Using embedding of a last token as a sequence embedding. '
-                           'If that is not desirable, change `pooling_strategy`')
-    return reduce_cls(model_outputs, mask_2d, cls_pos='tail')
-
-
 class TransformerTorchEncoder(TorchDevice, BaseEncoder):
     """
-    Internally, TransformerTorchEncoder wraps the tensorflow-version of transformers from huggingface.
+    Internally, TransformerTorchEncoder wraps the pytorch version of transformers from huggingface.
     """
 
     def __init__(
         self,
-        pretrained_model_name_or_path: str = 'bert-base-uncased',
-        pooling_strategy: str = 'auto',
+        pretrained_model_name_or_path: str = "bert-base-uncased",
+        base_model: str = None,
+        pooling_strategy: str = "cls",
+        layer_index: int = -1,
         max_length: Optional[int] = None,
-        truncation_strategy: str = 'longest_first',
+        truncation_strategy: bool = True,
         model_save_path: Optional[str] = None,
         *args,
-        **kwargs
+        **kwargs,
     ):
         """
         :param pretrained_model_name_or_path: Either:
-            - a string with the `shortcut name` of a pre-trained model to load from cache or download, e.g.: ``bert-base-uncased``.
-            - a string with the `identifier name` of a pre-trained model that was user-uploaded to Hugging Face S3, e.g.: ``dbmdz/bert-base-german-cased``.
+            - a string, the `model id` of a pretrained model hosted inside a model repo on huggingface.co, e.g.: ``bert-base-uncased``.
             - a path to a `directory` containing model weights saved using :func:`~transformers.PreTrainedModel.save_pretrained`, e.g.: ``./my_model_directory/``.
-            - a path or url to a `tensorflow index checkpoint file` (e.g. `./tf_model/model.ckpt.index`). In this case, ``from_tf`` should be set to True and a configuration object should be provided as ``config`` argument.
-            This loading path is slower than converting the TensorFlow
-            checkpoint in a PyTorch model using the provided conversion scripts and loading the PyTorch model afterwards.
+        :param base_model: The name of the base model to use for creating the tokenizer. If None, will be equal to `pretrained_model_name_or_path`.
         :param pooling_strategy: the strategy to merge the word embeddings into the chunk embedding. Supported
-            strategies include 'auto', 'cls', 'mean', 'max', 'min'.
+            strategies include 'cls', 'mean', 'max', 'min'.
+        :param layer_index: index of the transformer layer that is used to create encodings. Layer 0 corresponds to the embeddings layer
         :param max_length: the max length to truncate the tokenized sequences to.
         :param model_save_path: the path of the encoder model. If a valid path is given, the encoder will be saved to the given path
-        :param truncation_strategy: select truncation strategy. Supported values
-            * `True` or `'longest_first'` (default): truncate to a max length specified in `max_length` or to the max acceptable input length for the model if no length is provided (`max_length=None`).
-            * `'only_first'`:  This will only truncate the first sequence of a pair if a pair of sequences (or a batch of pairs) is provided
-            * `'only_second'`: This will only truncate the second sequence of a pair if a pair of sequences (or a batch of pairs) is provided
-            * `False` or `'do_not_truncate'`: No truncation (i.e. can output batch with sequences length greater than the model max admissible input size)
+        :param truncation_strategy: Truncation strategy
+            ..deprecated::
+                This parameter has no effect - ``truncation=True`` is always applied on tokenizing by default.
 
         ..warning::
             `model_save_path` should be relative to executor's workspace
@@ -68,99 +49,114 @@ class TransformerTorchEncoder(TorchDevice, BaseEncoder):
 
         super().__init__(*args, **kwargs)
         self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.base_model = base_model if base_model else pretrained_model_name_or_path
         self.pooling_strategy = pooling_strategy
         self.max_length = max_length
-        self.truncation_strategy = truncation_strategy
         self.model_save_path = model_save_path
+
+        if self.pooling_strategy == "auto":
+            self.pooling_strategy == "cls"
+            raise DeprecationWarning(
+                "'auto' pooling_strategy is deprecated, please use"
+                " 'cls' to maintain the old default behavior."
+            )
+
+        if self.pooling_strategy not in ["cls", "mean", "max", "min"]:
+            self.logger.error(
+                f"pooling strategy not found: {self.pooling_strategy}."
+                " The allowed pooling strategies are 'cls', 'mean', 'max', 'min'."
+            )
+            raise NotImplementedError
 
     def __getstate__(self):
         if self.model_save_path:
             if not os.path.exists(self.model_abspath):
-                self.logger.info(f'create folder for saving transformer models: {self.model_abspath}')
+                self.logger.info(
+                    f"create folder for saving transformer models: {self.model_abspath}"
+                )
                 os.mkdir(self.model_abspath)
             self.model.save_pretrained(self.model_abspath)
             self.tokenizer.save_pretrained(self.model_abspath)
         return super().__getstate__()
 
-    def array2tensor(self, array):
-        import torch
-        tensor = torch.tensor(array)
-        return tensor.cuda() if self.on_gpu else tensor
-
-    def tensor2array(self, tensor):
-        return tensor.cpu().numpy() if self.on_gpu else tensor.numpy()
-
     @property
     def model_abspath(self) -> str:
-        """Get the file path of the encoder model storage
-        """
+        """Get the file path of the encoder model storage"""
         return self.get_file_from_workspace(self.model_save_path)
 
     @cached_property
     def model(self):
-        from transformers import AutoModelForPreTraining
-        model = AutoModelForPreTraining.from_pretrained(self.pretrained_model_name_or_path)
+        from transformers import AutoModel
+        model = AutoModel.from_pretrained(
+            self.pretrained_model_name_or_path, output_hidden_states=True
+        )
         self.to_device(model)
         return model
 
     @cached_property
-    def no_gradients(self):
-        import torch
-        return torch.no_grad
-
-    @cached_property
-    def tensor_func(self):
-        import torch
-        return torch.tensor
-
-    @cached_property
     def tokenizer(self):
         from transformers import AutoTokenizer
-        tokenizer = AutoTokenizer.from_pretrained(self.pretrained_model_name_or_path)
+        tokenizer = AutoTokenizer.from_pretrained(self.base_model)
         return tokenizer
 
     @batching
     @as_ndarray
-    def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+    def encode(self, data: "np.ndarray", *args, **kwargs) -> "np.ndarray":
         """
         :param data: a 1d array of string type in size `B`
         :return: an ndarray in size `B x D`
         """
-        try:
-            if self.tokenizer.pad_token is None:
-                self.tokenizer.pad_token = self.tokenizer.eos_token
-                if self.tokenizer.pad_token is None:
-                    self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-            ids_info = self.tokenizer.batch_encode_plus(data,
-                                                        max_length=self.max_length,
-                                                        truncation=self.truncation_strategy,
-                                                        pad_to_max_length=True)
-        except ValueError:
-            self.model.resize_token_embeddings(len(self.tokenizer))
-            ids_info = self.tokenizer.batch_encode_plus(data,
-                                                        max_length=self.max_length,
-                                                        pad_to_max_length=True)
-        token_ids_batch = self.array2tensor(ids_info['input_ids'])
-        mask_ids_batch = self.array2tensor(ids_info['attention_mask'])
-        with self.no_gradients():
-            outputs = self.model(token_ids_batch,
-                                 attention_mask=mask_ids_batch,
-                                 output_hidden_states=True)
+        import torch
+        torch.set_grad_enabled(False)
 
-            hidden_states = outputs[-1]
-            output_embeddings = hidden_states[-1]
-            _mask_ids_batch = self.tensor2array(mask_ids_batch)
-            _seq_output = self.tensor2array(output_embeddings)
-            if self.pooling_strategy == 'auto':
-                output = auto_reduce(_seq_output, _mask_ids_batch, self.model.base_model_prefix)
-            elif self.pooling_strategy == 'mean':
-                output = reduce_mean(_seq_output, _mask_ids_batch)
-            elif self.pooling_strategy == 'max':
-                output = reduce_max(_seq_output, _mask_ids_batch)
-            elif self.pooling_strategy == 'min':
-                output = reduce_min(_seq_output, _mask_ids_batch)
-            else:
-                self.logger.error(f'pooling strategy not found: {self.pooling_strategy}')
-                raise NotImplementedError
-        return output
+        if not self.tokenizer.pad_token:
+            self.tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+            self.model.resize_token_embeddings(len(self.tokenizer.vocab))
 
+        input_tokens = self.tokenizer(
+            list(data),
+            max_length=self.max_length,
+            padding="longest",
+            truncation=True,
+            return_tensors="pt",
+        )
+        input_tokens = {k: v.to(self.device) for k, v in input_tokens.items()}
+        outputs = self.model(**input_tokens)
+
+        n_layers = len(outputs.hidden_states)
+        if self.layer_index not in list(range(-n_layers, n_layers)):
+            self.logger.error(
+                f"Invalid value {self.layer_index} for `layer_index`,"
+                f" for the model {self.pretrained_model_name_or_path}"
+                f" valid values are integers from {-n_layers} to {n_layers-1}."
+            )
+            raise ValueError
+
+        if self.pooling_strategy == "cls" and not self.tokenizer.cls_token:
+            self.logger.error(
+                f"You have set pooling_strategy to 'cls', but the tokenizer"
+                f" for the model {self.pretrained_model_name_or_path}"
+                f" does not have a cls token set."
+            )
+            raise ValueError
+
+        fill_vals = {"cls": 0.0, "mean": 0.0, "max": -np.inf, "min": np.inf}
+        fill_val = torch.tensor(fill_vals[self.pooling_strategy], device=self.device)
+
+        layer = outputs.hidden_states[self.layer_index]
+        attn_mask = input_tokens["attention_mask"].unsqueeze(-1).expand_as(layer)
+        layer = torch.where(attn_mask.bool(), layer, fill_val)
+
+        if self.pooling_strategy == "cls":
+            CLS = self.tokenizer.cls_token_id
+            ind = torch.nonzero(input_tokens['input_ids'] == CLS)[:, 1]
+            ind = ind.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, layer.shape[2])
+            embeddings = torch.gather(layer, 1, ind).squeeze()
+        elif self.pooling_strategy == "mean":
+            embeddings = layer.sum(dim=1) / attn_mask.sum(dim=1)
+        elif self.pooling_strategy == "max":
+            embeddings = layer.max(dim=1).values
+        elif self.pooling_strategy == "min":
+            embeddings = layer.min(dim=1).values
+
+        return embeddings.cpu().numpy()
--- encoders/nlp/TransformerTorchEncoder/tests/test_transformertorchencoder.py
+++ encoders/nlp/TransformerTorchEncoder/tests/test_transformertorchencoder.py
@@ -1,113 +1,144 @@
 import os
-import numpy as np
-import pytest
+from itertools import product
 import random
 import string
-from .. import TransformerTorchEncoder
+
+import numpy as np
+import pytest
 from jina.executors import BaseExecutor
 from jina.executors.metas import get_default_metas
 
-
-@pytest.fixture(scope='function')
-def random_workspace_name():
-    """Generate a random workspace name with digits and letters."""
-    rand = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    return f'JINA_TEST_WORKSPACE_{rand}'
+from .. import TransformerTorchEncoder
 
 
-@pytest.fixture(scope='function')
-def test_metas(tmpdir, random_workspace_name):
-    os.environ[random_workspace_name] = str(tmpdir)
+@pytest.fixture(scope="function")
+def test_metas(tmp_path):
     metas = get_default_metas()
-    metas['workspace'] = os.environ[random_workspace_name]
-    if 'JINA_TEST_GPU' in os.environ:
-        metas['on_gpu'] = True
+    metas["workspace"] = str(tmp_path)
+    if "JINA_TEST_GPU" in os.environ:
+        metas["on_gpu"] = True
     yield metas
-    del os.environ[random_workspace_name]
-
-
-encoders_parameters = [
-    {
-        "pretrained_model_name_or_path": 'bert-base-uncased',
-        "model_save_path": 'bert-base-uncased',
-    },
-    {
-        "pooling_strategy": 'mean',
-        "pretrained_model_name_or_path": 'bert-base-uncased',
-        "model_save_path": 'bert-base-uncased-mean',
-    },
-    {
-        "pooling_strategy": 'min',
-        "pretrained_model_name_or_path": 'bert-base-uncased',
-        "model_save_path": 'bert-base-uncased-min',
-    },
-    {
-        "pooling_strategy": 'max',
-        "pretrained_model_name_or_path": 'bert-base-uncased',
-        "model_save_path": 'bert-base-uncased-max',
-    },
-    {
-        "pretrained_model_name_or_path": 'xlnet-base-cased',
-        "model_save_path": 'xlnet-base-cased',
-    },
-    {
-        "pooling_strategy": 'mean',
-        "pretrained_model_name_or_path": 'xlnet-base-cased',
-        "model_save_path": 'xlnet-base-cased-mean',
-    },
-    {
-        "pooling_strategy": 'min',
-        "pretrained_model_name_or_path": 'xlnet-base-cased',
-        "model_save_path": 'xlnet-base-cased-min',
-    },
-    {
-        "pooling_strategy": 'max',
-        "pretrained_model_name_or_path": 'xlnet-base-cased',
-        "model_save_path": 'xlnet-base-cased-max',
-    }
-]
 
 
 @pytest.fixture
 def encoder(request, test_metas):
+    request.param["model_save_path"] = (
+        request.param["pretrained_model_name_or_path"].replace(
+            "sentence-transformers/", ""
+        )
+        + f'-{request.param["pooling_strategy"]}'
+    )
     return TransformerTorchEncoder(metas=test_metas, **request.param)
 
 
-@pytest.mark.parametrize('encoder', encoders_parameters, indirect=['encoder'])
+_params_dict = {
+    "pretrained_model_name_or_path": [
+        "sentence-transformers/distilbert-base-nli-stsb-mean-tokens",
+        "sentence-transformers/bert-base-nli-stsb-mean-tokens",
+        "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens",
+        "sentence-transformers/roberta-base-nli-stsb-mean-tokens",
+        "xlnet-base-cased",
+    ],
+    "pooling_strategy": ["cls", "mean", "max"],
+    "layer_index": [-1, -2, 0],
+}
+
+# Create a cartesian product of the param lists
+encoder_params = [
+    dict(zip(_params_dict.keys(), vals)) for vals in product(*_params_dict.values())
+]
+encoder_params_ind_1 = filter(lambda x: x["layer_index"] == -1, encoder_params)
+
+
+def _assert_params_equal(params_dict: dict, encoder: TransformerTorchEncoder):
+    for key, val in params_dict.items():
+        assert val == getattr(encoder, key)
+
+
+@pytest.mark.parametrize("encoder", encoder_params, indirect=True)
 def test_encoding_results(encoder):
-    target_output_dim = 768
-    test_data = np.array(['it is a good day!', 'the dog sits on the floor.'])
+    test_data = np.array(["it is a good day!", "the dog sits on the floor."])
     encoded_data = encoder.encode(test_data)
-    assert encoded_data.shape == (2, target_output_dim)
-    assert not np.allclose(encoded_data[0], encoded_data[1])
+
+    hidden_dim_sizes = {
+        "sentence-transformers/distilbert-base-nli-stsb-mean-tokens": 768,
+        "sentence-transformers/bert-base-nli-stsb-mean-tokens": 768,
+        "sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens": 768,
+        "sentence-transformers/roberta-base-nli-stsb-mean-tokens": 768,
+        "xlnet-base-cased": 768,
+    }
+    hidden_dim_size = hidden_dim_sizes[encoder.pretrained_model_name_or_path]
+    assert encoded_data.shape == (2, hidden_dim_size)
+
+    if encoder.pooling_strategy != "cls" or encoder.layer_index != 0:
+        assert not np.allclose(encoded_data[0], encoded_data[1], rtol=1)
+    else:
+        assert np.allclose(encoded_data[0], encoded_data[1], atol=1e-5, rtol=1e-4)
 
 
-@pytest.mark.parametrize('encoder', encoders_parameters, indirect=['encoder'])
-def test_save_and_load(encoder):
+@pytest.mark.parametrize("encoder", encoder_params_ind_1, indirect=True)
+def test_max_length_truncation(encoder):
+    encoder.max_length = 3
+    test_data = np.array(["it is a very good day!", "it is a very sunny day!"])
+    encoded_data = encoder.encode(test_data)
+
+    np.testing.assert_allclose(encoded_data[0], encoded_data[1], atol=1e-5, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "encoder, params", list(zip(encoder_params, encoder_params)), indirect=["encoder"]
+)
+def test_save_and_load(encoder, params):
     encoder.save_config()
+    _assert_params_equal(params, encoder)
     assert os.path.exists(encoder.config_abspath)
-    test_data = np.array(['a', 'b', 'c', 'x', '!'])
+
+    test_data = np.array(["a", "b", "c", "x", "!"])
     encoded_data_control = encoder.encode(test_data)
+
     encoder.touch()
     encoder.save()
     assert os.path.exists(encoder.save_abspath)
+
     encoder_loaded = BaseExecutor.load(encoder.save_abspath)
+    _assert_params_equal(params, encoder_loaded)
+
     encoded_data_test = encoder_loaded.encode(test_data)
-    assert encoder_loaded.max_length == encoder.max_length
     np.testing.assert_array_equal(encoded_data_control, encoded_data_test)
 
 
-@pytest.mark.parametrize('encoder', encoders_parameters, indirect=['encoder'])
-def test_save_and_load_config(encoder):
+@pytest.mark.parametrize(
+    "encoder, params", list(zip(encoder_params, encoder_params)), indirect=["encoder"]
+)
+def test_save_and_load_config(encoder, params):
     encoder.save_config()
+    _assert_params_equal(params, encoder)
     assert os.path.exists(encoder.config_abspath)
+
     encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-    assert encoder_loaded.max_length == encoder.max_length
+    _assert_params_equal(params, encoder_loaded)
 
 
-@pytest.mark.parametrize('encoder', encoders_parameters[5:6], indirect=['encoder'])
-def test_parameter_override(encoder):
-    encoder_preset = encoders_parameters[5]
-    assert encoder.pretrained_model_name_or_path == encoder_preset['pretrained_model_name_or_path']
-    assert encoder.pooling_strategy == encoder_preset['pooling_strategy']
-    assert encoder.model_save_path == encoder_preset['model_save_path']
+@pytest.mark.parametrize("layer_index", [-100, 100])
+@pytest.mark.parametrize("encoder", encoder_params[0], indirect=["encoder"])
+def test_wrong_layer_index(encoder, layer_index):
+    encoder.layer_index = layer_index
+    test_data = np.array(["it is a good day!", "the dog sits on the floor."])
+    with pytest.raises(ValueError):
+        encoded_data = encoder.encode(test_data)
+
+
+def test_wrong_pooling_strategy():
+    with pytest.raises(NotImplementedError):
+        TransformerTorchEncoder(pooling_strategy="wrong")
+
+
+@pytest.mark.parametrize(
+    "encoder",
+    [{"pooling_strategy": "cls", "pretrained_model_name_or_path": "gpt2"}],
+    indirect=True,
+)
+def test_no_cls_token(encoder):
+    test_data = np.array(["it is a good day!", "the dog sits on the floor."])
+    with pytest.raises(ValueError):
+        encoder.encode(test_data)
