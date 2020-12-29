a0a766f6b1cda7896242b6a4823e94ccff251e33
Joan Fontanals <joan.martinez@jina.ai>
2020-12-29 11:21:38 +0100

    fix: fix reciprocal rank evaluator (#1482)

--- evaluators/rank/ReciprocalRankEvaluator/tests/test_reciprocalrankevaluator.py
+++ evaluators/rank/ReciprocalRankEvaluator/tests/test_reciprocalrankevaluator.py
@@ -4,16 +4,28 @@ from .. import ReciprocalRankEvaluator
 
 
 @pytest.mark.parametrize('actual, desired, score',
-[
-    ([],[], 0.0),
-    ([1, 2, 3, 4],[], 0.0),
-    ([],[1, 2, 3, 4], 0.0),
-    ([1, 2, 3, 4],[1, 2, 3, 4], 1.0),
-    ([1, 2, 3, 4],[2, 1, 3, 4], 0.5),
-    ([1, 2, 3, 4],[11, 1, 2, 3], 0.0),
-    ([4, 2, 3, 1],[1, 2, 3, 4], 0.0),
-]
-)
+                         [
+                             ([], [], 0.0),
+                             ([1, 2, 3, 4], [], 0.0),
+                             ([], [1, 2, 3, 4], 0.0),
+                             ([1, 2, 3, 4], [1, 2, 3, 4], 1.0),
+                             ([1, 2, 3, 4], [2, 1, 3, 4], 0.5),
+                             ([1, 2, 3, 4], [11, 1, 2, 3], 0.0),
+                             ([4, 2, 3, 1], [1, 2, 3, 4], 0.0),
+                         ]
+                         )
 def test_reciprocalrankevaluator(actual, desired, score):
     evaluator = ReciprocalRankEvaluator(eval_at=3)
+    assert evaluator.evaluate(actual, desired) == score
+
+
+@pytest.mark.parametrize('actual, desired, score',
+                         [
+                             ([1, 2, 3, 4], [1, 2, 3, 4], 1.0),
+                             ([1, 2, 3, 4], [11, 1, 2, 3], 0.0),
+                             ([4, 2, 3, 1], [1, 2, 3, 4], 0.25),
+                         ]
+                         )
+def test_reciprocalrankevaluator_None(actual, desired, score):
+    evaluator = ReciprocalRankEvaluator(eval_at=None)
     assert evaluator.evaluate(actual, desired) == score
