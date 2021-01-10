0e24b5b020e22fb80464662f7d2ef603344fb26d
carlosb1 <mcflurry0@gmail.com>
2021-01-10 18:23:20 +0100

    feat: add cmlm encoder

--- encoders/nlp/UniversalSentenceEncoder/__init__.py
+++ encoders/nlp/UniversalSentenceEncoder/__init__.py
@@ -1,6 +1,8 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+from typing import Optional
+
 import numpy as np
 
 from jina.executors.decorators import batching, as_ndarray
@@ -17,21 +19,27 @@ class UniversalSentenceEncoder(BaseTFEncoder):
     def __init__(
             self,
             model_url: str = 'https://tfhub.dev/google/universal-sentence-encoder/4',
-            *args,
+            preprocessor_url: Optional[str] = None,
+            * args,
             **kwargs):
         """
         :param model_url: the url of the model (TensorFlow Hub). For supported models see
                           family overview: https://tfhub.dev/google/collections/universal-sentence-encoder/1)
+        :param preprocessor_url: the url of preprocessors (TensorFlow Hub).
         :param args:
         :param kwargs:
         """
         super().__init__(*args, **kwargs)
         self.model_url = model_url
+        self.preprocessor_url = preprocessor_url
 
     def post_init(self):
         self.to_device()
         import tensorflow_hub as hub
-        self.model = hub.load(self.model_url)
+        self.preprocessor = None
+        if self.preprocessor_url:
+            self.preprocessor = hub.KerasLayer(self.preprocessor_url)
+        self.model = hub.KerasLayer(self.model_url)
 
     @batching
     @as_ndarray
@@ -43,4 +51,6 @@ class UniversalSentenceEncoder(BaseTFEncoder):
         :param kwargs:
         :return: an ndarray in size `B x D`
         """
+        if self.preprocessor:
+            data = self.preprocessor(data)
         return self.model(data)
