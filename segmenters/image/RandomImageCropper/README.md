e67c55f8af0c5bf14c87f2d4f90500bf13774129
Joan Fontanals <joan.martinez@jina.ai>
2021-01-11 18:37:41 +0100

    feat: add set of image segmenters (#2165)
    * feat: add set of image segmenters
    
    * feat: add proper jina image

---
+++ segmenters/image/RandomImageCropper/README.md
@@ -0,0 +1,3 @@
+# RandomImageCropper
+
+crops the image with a random crop box. The coordinate is the same coordinate-system 
---
+++ segmenters/image/RandomImageCropper/__init__.py
@@ -0,0 +1,45 @@
+from typing import Tuple, Dict, List, Union
+
+import numpy as np
+from jina.executors.segmenters import BaseSegmenter
+
+from .helper import _crop_image, _move_channel_axis, _load_image
+
+
+class RandomImageCropper(BaseSegmenter):
+    """
+    :class:`RandomImageCropper` crops the image with a random crop box. The coordinate is the same coordinate-system
+        in the :py:mode:`PIL.Image`.
+    """
+
+    def __init__(self,
+                 target_size: Union[Tuple[int], int] = 224,
+                 num_patches: int = 1,
+                 channel_axis: int = -1,
+                 *args,
+                 **kwargs):
+        """
+
+        :param target_size: desired output size. If size is a sequence like (h, w), the output size will be matched to
+            this. If size is an int, the output will have the same height and width as the `target_size`.
+        """
+        super().__init__(*args, **kwargs)
+        self.target_size = target_size
+        self.num_patches = num_patches
+        self.channel_axis = channel_axis
+
+    def segment(self, blob: 'np.ndarray', *args, **kwargs) -> List[Dict]:
+        """
+        Crop the input image array.
+
+        :param blob: the ndarray of the image
+        :return: a list of chunk dicts with the cropped images
+        """
+        raw_img = _load_image(blob, self.channel_axis)
+        result = []
+        for i in range(self.num_patches):
+            _img, top, left = _crop_image(raw_img, self.target_size, how='random')
+            img = _move_channel_axis(np.asarray(_img), -1, self.channel_axis)
+            result.append(
+                dict(offset=0, weight=1., blob=np.asarray(img).astype('float32'), location=(top, left)))
+        return result
