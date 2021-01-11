c4a6be807053c4d04acb0a474c77a82ebcdf4210
Joan Fontanals <joan.martinez@jina.ai>
2021-01-11 18:41:56 +0100

    feat: add set of nlp segmenters (#2160)

---
+++ segmenters/nlp/DeepSegmenter/tests/test_deepsegmenter.py
@@ -0,0 +1,10 @@
+from .. import DeepSegmenter
+
+
+def test_deepsegmenter():
+    crafter = DeepSegmenter()
+    text = 'I am Batman i live in gotham'
+    chunks = crafter.segment(text, 0)
+    assert len(chunks) == 2
+    assert chunks[0]['text'] == 'I am Batman'
+    assert chunks[1]['text'] == 'i live in gotham'
---
+++ segmenters/nlp/JiebaSegmenter/README.md
@@ -0,0 +1,3 @@
+# JiebaSegmenter
+
+split the chinese text on the doc-level into words on the chunk-level with `jieba`. 
