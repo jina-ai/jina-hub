e26f3450e7376df033dcc0171965f099da7fd4dd
carlosb1 <mcflurry0@gmail.com>
2021-01-24 18:46:47 +0100

    fix: added test for 2d gray imgs

--- crafters/image/ImageResizer/tests/test_imageresizer.py
+++ crafters/image/ImageResizer/tests/test_imageresizer.py
@@ -11,6 +11,11 @@ def create_random_gray_img_array(img_height, img_width):
     return np.random.randint(0, 256, (img_height, img_width, 1))
 
 
+def create_random_gray_img_array_2d(img_height, img_width):
+    import numpy as np
+    return np.random.randint(0, 256, (img_height, img_width))
+
+
 def test_resize():
     img_width = 20
     img_height = 17
@@ -45,5 +50,24 @@ def test_resize_gray():
     output_dim = (img_height, img_width)
     crafter = ImageResizer(target_size=output_dim)
     img_array = create_random_gray_img_array(img_width, img_height)
+    crafted_doc = crafter.craft(img_array)
+    assert crafted_doc['blob'].shape == output_dim
+
+
+def test_resize_gray_2d():
+    img_width = 20
+    img_height = 17
+
+    # Test for int target_size
+    output_dim = 71
+    crafter = ImageResizer(target_size=output_dim)
+    img_array = create_random_gray_img_array_2d(img_height, img_width)
+    crafted_doc = crafter.craft(img_array)
+    assert min(crafted_doc['blob'].shape[:-1]) == output_dim
+
+    # Test for tuple/list target_size
+    output_dim = (img_height, img_width)
+    crafter = ImageResizer(target_size=output_dim)
+    img_array = create_random_gray_img_array_2d(img_width, img_height)
     crafted_doc = crafter.craft(img_array)
     assert crafted_doc['blob'].shape == output_dim
