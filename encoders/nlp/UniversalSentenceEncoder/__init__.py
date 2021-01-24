2486777b23111f7fe937263de3a99bc495d05b1e
Carlos B <carlos.baezruiz@gmail.com>
2021-01-24 17:42:02 +0100

    feat:  new cmlm encoder (#2202)
    * feat: add dependencies
    
    * feat: add impl cmlm encoder
    
    * feat: added tests
    
    * feat: updated manifest
    
    * feat: changed version
    
    * feat: removed not used dep
    
    * feat: create constant to choose encoder
    
    * feat: move var to constant
    
    * feat: added constants to verify encoders
    
    * feat: refactor code for sent encoder
    
    * feat: add tests for cmlm encoder
    
    * feat: doc private classes
    
    * feat: add tests for cmlm preprocessor and encoder
    
    * feat: removed space for a good tab
    
    * feat: fixed error in test in mock
    
    * feat: moved post_init functions
    
    * feat: removed post_init
    
    * feat: removed not used dependency

--- encoders/nlp/UniversalSentenceEncoder/__init__.py
+++ encoders/nlp/UniversalSentenceEncoder/__init__.py
@@ -1,10 +1,62 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+from typing import Optional
+
+import tensorflow_hub as hub
 import numpy as np
 
 from jina.executors.decorators import batching, as_ndarray
 from jina.executors.encoders.frameworks import BaseTFEncoder
+
+
+UNIVERSAL_SENTENCE_ENCODER = 'https://tfhub.dev/google/universal-sentence-encoder/4'
+MODEL_ENCODER_CMLM = "https://tfhub.dev/google/universal-sentence-encoder-cmlm/en-base/1"
+PREPROCESOR_CMLM = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2"
+
+
+class CMLMEncoder:
+    """
+    :class:`CMLMEncoder` is an private class  encoder to represent a CMLM
+    Universal Sentence Encoder family
+    (https://tfhub.dev/google/universal-sentence-encoder-cmlm/en-base/1).
+    It encodes data from an 1d array of string in size `B` into an ndarray in size `B x D`.
+    """
+
+    def __init__(self):
+        import tensorflow_text as text
+        self.bert_preprocessor = hub.KerasLayer(PREPROCESOR_CMLM)
+        self.encoder = hub.KerasLayer(MODEL_ENCODER_CMLM)
+
+    def encode(self, data: 'np.ndarray') -> 'np.ndarray':
+        """
+        :param data: a 1d array of string type in size `B`
+        :param args:
+        :param kwargs:
+        :return: an ndarray in size `B x D`
+        """
+        return self.encoder(self.bert_preprocessor(data))['default']
+
+
+class GeneralEncoder:
+    """
+    :class:`GeneralEncoder` is general universal sentence encoder
+    which load a model and it encodes from an 1d array of string
+    in size `B` into an ndarray in size `B x D`.
+    """
+
+    def __init__(self, model_url: str):
+        self.model_url = model_url
+        self.model = hub.load(self.model_url)
+
+    def encode(self, data: 'np.ndarray') -> 'np.ndarray':
+        """
+        :param data: a 1d array of string type in size `B`
+        :param args:
+        :param kwargs:
+        :return: an ndarray in size `B x D`
+        """
+        return self.model(data)
 
 
 class UniversalSentenceEncoder(BaseTFEncoder):
@@ -16,8 +68,8 @@ class UniversalSentenceEncoder(BaseTFEncoder):
 
     def __init__(
             self,
-            model_url: str = 'https://tfhub.dev/google/universal-sentence-encoder/4',
-            *args,
+            model_url: str = UNIVERSAL_SENTENCE_ENCODER,
+            * args,
             **kwargs):
         """
         :param model_url: the url of the model (TensorFlow Hub). For supported models see
@@ -30,17 +82,19 @@ class UniversalSentenceEncoder(BaseTFEncoder):
 
     def post_init(self):
         self.to_device()
-        import tensorflow_hub as hub
-        self.model = hub.load(self.model_url)
+
+        if self.model_url == MODEL_ENCODER_CMLM:
+            self.sentence_encoder = CMLMEncoder()
+        else:
+            self.sentence_encoder = GeneralEncoder(self.model_url)
 
     @batching
     @as_ndarray
     def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
         """
-
         :param data: a 1d array of string type in size `B`
         :param args:
         :param kwargs:
         :return: an ndarray in size `B x D`
         """
-        return self.model(data)
+        return self.sentence_encoder.encode(data)
--- encoders/nlp/UniversalSentenceEncoder/tests/test_universalsentenceencoder.py
+++ encoders/nlp/UniversalSentenceEncoder/tests/test_universalsentenceencoder.py
@@ -1,12 +1,14 @@
 import os
 import mock
+from mock import MagicMock
 import numpy as np
 import shutil
 import pytest
 
-from .. import UniversalSentenceEncoder
+from .. import UniversalSentenceEncoder, MODEL_ENCODER_CMLM
 from jina.executors import BaseExecutor
 from jina.executors.metas import get_default_metas
+
 
 def get_metas():
     metas = get_default_metas()
@@ -26,13 +28,32 @@ def rm_files(tmp_files):
 
 class MockModule:
     def __call__(self, data, *args, **kwargs):
-        print('i am a mocker function')
         assert len(data.shape) == 1
         return np.stack([[i for i in range(target_output_dim)]] * data.shape[0])
 
 
+class MockPreprocessorCMLM:
+    def __call__(self, data, *args, **kwargs):
+        result = {}
+        assert len(data.shape) == 1
+        result['input_word_ids'] = np.stack(
+            [[i for i in range(target_output_dim_cmlm)]] * data.shape[0])
+        return result
+
+
+class MockEncoderCMLM:
+    def __call__(self, data, *args, **kwargs):
+        result = {}
+        assert len(data['input_word_ids'].shape) == 2
+        result['default'] = np.empty(
+            shape=(2, data['input_word_ids'].shape[1]))
+        return result
+
+
 target_output_dim = 512
+target_output_dim_cmlm = 768
 test_data = np.array(['it is a good day!', 'the dog sits on the floor.'])
+
 
 def _test_encoding_results():
     metas = get_metas()
@@ -41,17 +62,32 @@ def _test_encoding_results():
     assert encoded_data.shape == (2, target_output_dim)
 
 
-@pytest.mark.skipif('JINA_TEST_PRETRAINED' not in os.environ, reason='skip the pretrained test if not set')
+def _test_cmlm_encoding_results():
+    metas = get_metas()
+    encoder = UniversalSentenceEncoder(
+        metas=metas, model_url=MODEL_ENCODER_CMLM)
+    encoder_data = encoder.encode(test_data)
+    assert encoder_data.shape == (2, target_output_dim_cmlm)
+
+
+@ pytest.mark.skipif('JINA_TEST_PRETRAINED' not in os.environ, reason='skip the pretrained test if not set')
 def test_encoding_result():
     _test_encoding_results()
+    _test_cmlm_encoding_results()
 
 
-@mock.patch('tensorflow_hub.load', return_value=MockModule())
+@ mock.patch('tensorflow_hub.load', return_value=MockModule())
 def test_encoding_result_local(mocker):
     _test_encoding_results()
 
 
-@mock.patch('tensorflow_hub.load', return_value=MockModule())
+@ mock.patch('tensorflow_hub.KerasLayer')
+def test_encoding_result_local_cmlm(mocker):
+    mocker.side_effect = [MockPreprocessorCMLM(), MockEncoderCMLM()]
+    _test_cmlm_encoding_results()
+
+
+@ mock.patch('tensorflow_hub.load', return_value=MockModule())
 def test_save_and_load(mocker):
     metas = get_metas()
     encoder = UniversalSentenceEncoder(metas=metas)
@@ -66,7 +102,7 @@ def test_save_and_load(mocker):
     rm_files([encoder.save_abspath])
 
 
-@mock.patch('tensorflow_hub.load', return_value=MockModule())
+@ mock.patch('tensorflow_hub.load', return_value=MockModule())
 def test_save_and_load_config(mocker):
     metas = get_metas()
     encoder = UniversalSentenceEncoder(metas=metas)
@@ -75,3 +111,20 @@ def test_save_and_load_config(mocker):
     encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
     assert encoder_loaded.model_url == encoder.model_url
     rm_files([encoder.config_abspath])
+
+
+@ mock.patch('tensorflow_hub.load', return_value=MockModule())
+def test_get_universal_sentence_encoder(mocker):
+    MODEL_ENCODER = 'https://tfhub.dev/google/universal-sentence-encoder/4'
+    metas = get_metas()
+    encoder = UniversalSentenceEncoder(metas=metas)
+    assert encoder.model_url == MODEL_ENCODER
+
+
+@ mock.patch('tensorflow_hub.KerasLayer')
+def test_get_universal_sentence_encoder_mlcm(mocker):
+    mocker.side_effect = [MockPreprocessorCMLM(), MockEncoderCMLM()]
+    metas = get_metas()
+    encoder = UniversalSentenceEncoder(
+        metas=metas, model_url=MODEL_ENCODER_CMLM)
+    assert encoder.model_url == MODEL_ENCODER_CMLM
