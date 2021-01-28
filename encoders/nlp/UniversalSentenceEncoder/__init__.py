c14b58946ad05127523f412de83090444a3bae7f
Joan Fontanals <joan.martinez@jina.ai>
2021-01-28 08:53:55 +0100

    fix: fix warning Use (#2777)

--- encoders/nlp/UniversalSentenceEncoder/__init__.py
+++ encoders/nlp/UniversalSentenceEncoder/__init__.py
@@ -1,62 +1,15 @@
 __copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Optional
-
 import tensorflow_hub as hub
 import numpy as np
 
 from jina.executors.decorators import batching, as_ndarray
 from jina.executors.encoders.frameworks import BaseTFEncoder
 
-
 UNIVERSAL_SENTENCE_ENCODER = 'https://tfhub.dev/google/universal-sentence-encoder/4'
 MODEL_ENCODER_CMLM = "https://tfhub.dev/google/universal-sentence-encoder-cmlm/en-base/1"
 PREPROCESOR_CMLM = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2"
-
-
-class CMLMEncoder:
-    """
-    :class:`CMLMEncoder` is an private class  encoder to represent a CMLM
-    Universal Sentence Encoder family
-    (https://tfhub.dev/google/universal-sentence-encoder-cmlm/en-base/1).
-    It encodes data from an 1d array of string in size `B` into an ndarray in size `B x D`.
-    """
-
-    def __init__(self):
-        import tensorflow_text as text
-        self.bert_preprocessor = hub.KerasLayer(PREPROCESOR_CMLM)
-        self.encoder = hub.KerasLayer(MODEL_ENCODER_CMLM)
-
-    def encode(self, data: 'np.ndarray') -> 'np.ndarray':
-        """
-        :param data: a 1d array of string type in size `B`
-        :param args:
-        :param kwargs:
-        :return: an ndarray in size `B x D`
-        """
-        return self.encoder(self.bert_preprocessor(data))['default']
-
-
-class GeneralEncoder:
-    """
-    :class:`GeneralEncoder` is general universal sentence encoder
-    which load a model and it encodes from an 1d array of string
-    in size `B` into an ndarray in size `B x D`.
-    """
-
-    def __init__(self, model_url: str):
-        self.model_url = model_url
-        self.model = hub.load(self.model_url)
-
-    def encode(self, data: 'np.ndarray') -> 'np.ndarray':
-        """
-        :param data: a 1d array of string type in size `B`
-        :param args:
-        :param kwargs:
-        :return: an ndarray in size `B x D`
-        """
-        return self.model(data)
 
 
 class UniversalSentenceEncoder(BaseTFEncoder):
@@ -66,10 +19,52 @@ class UniversalSentenceEncoder(BaseTFEncoder):
     It encodes data from an 1d array of string in size `B` into an ndarray in size `B x D`.
     """
 
+    class GeneralEncoder:
+        """
+        :class:`GeneralEncoder` is general universal sentence encoder
+        which load a model and it encodes from an 1d array of string
+        in size `B` into an ndarray in size `B x D`.
+        """
+
+        def __init__(self, model_url: str):
+            self.model_url = model_url
+            self.model = hub.load(self.model_url)
+
+        def encode(self, data: 'np.ndarray') -> 'np.ndarray':
+            """
+            :param data: a 1d array of string type in size `B`
+            :param args:
+            :param kwargs:
+            :return: an ndarray in size `B x D`
+            """
+            return self.model(data)
+
+    class CMLMEncoder:
+        """
+        :class:`CMLMEncoder` is an private class  encoder to represent a CMLM
+        Universal Sentence Encoder family
+        (https://tfhub.dev/google/universal-sentence-encoder-cmlm/en-base/1).
+        It encodes data from an 1d array of string in size `B` into an ndarray in size `B x D`.
+        """
+
+        def __init__(self):
+            import tensorflow_text as text
+            self.bert_preprocessor = hub.KerasLayer(PREPROCESOR_CMLM)
+            self.encoder = hub.KerasLayer(MODEL_ENCODER_CMLM)
+
+        def encode(self, data: 'np.ndarray') -> 'np.ndarray':
+            """
+            :param data: a 1d array of string type in size `B`
+            :param args:
+            :param kwargs:
+            :return: an ndarray in size `B x D`
+            """
+            return self.encoder(self.bert_preprocessor(data))['default']
+
     def __init__(
             self,
             model_url: str = UNIVERSAL_SENTENCE_ENCODER,
-            * args,
+            *args,
             **kwargs):
         """
         :param model_url: the url of the model (TensorFlow Hub). For supported models see
@@ -84,9 +79,9 @@ class UniversalSentenceEncoder(BaseTFEncoder):
         self.to_device()
 
         if self.model_url == MODEL_ENCODER_CMLM:
-            self.sentence_encoder = CMLMEncoder()
+            self.sentence_encoder = UniversalSentenceEncoder.CMLMEncoder()
         else:
-            self.sentence_encoder = GeneralEncoder(self.model_url)
+            self.sentence_encoder = UniversalSentenceEncoder.GeneralEncoder(self.model_url)
 
     @batching
     @as_ndarray
