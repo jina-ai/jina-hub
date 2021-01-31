b43e3b2107a11ec19e360bf8bcfef00ea4f32acc
Florian Hönicke <hoenicke.florian@gmail.com>
2021-01-31 18:32:35 +0100

    docs: milvusindexer crud

--- indexers/vector/MilvusIndexer/__init__.py
+++ indexers/vector/MilvusIndexer/__init__.py
@@ -26,29 +26,48 @@ class MilvusIndexer(BaseVectorIndexer):
         self.index_params = index_params
 
     def post_init(self):
+        """Setup database handler.
+        """
         from milvusdbhandler import MilvusDBHandler
         super().post_init()
         self.milvus = MilvusDBHandler(self.host, self.port, self.collection_name)
 
     def get_query_handler(self):
+        """Get Milvus connector and build index.
+        """
         db_handler = self.milvus.connect()
         db_handler.build_index(self.index_type, self.index_params)
         return db_handler
 
     def get_add_handler(self):
+        """Get Milvus connector.
+        """
         return self.milvus.connect()
 
     def get_create_handler(self):
+        """Get Milvus connector.
+        """
         return self.milvus.connect()
 
     @property
     def query_handler(self):
+        """Get Milvus connector.
+        """
         return self.get_query_handler()
 
     def add(self, keys: 'np.ndarray', vectors: 'np.ndarray', *args, **kwargs):
+        """Add the embeddings and document ids to the index.
+
+        :param keys: document ids
+        :param vectors: embeddings
+        """
         self._validate_key_vector_shapes(keys, vectors)
         self.write_handler.insert(keys, vectors)
 
     def query(self, keys: 'np.ndarray', top_k: int, *args, **kwargs) -> Tuple['np.ndarray', 'np.ndarray']:
+        """Find the top-k vectors with smallest ``metric`` and return their ids in ascending order.
+        :param keys: numpy array containing vectors to search for
+        :param top_k: upper limit of responses for each search vector
+        """
         dist, ids = self.query_handler.search(keys, top_k, *args, **kwargs)
         return np.array(ids), np.array(dist)
