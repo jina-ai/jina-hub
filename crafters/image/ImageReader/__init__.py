ce3d097b53812a3546ef22079da565a1c193cb40
Susana Guzman <zuxi.w13@gmail.com>
2021-02-15 15:24:11 +0100

    docs: docstrings for ImageReader

--- crafters/image/ImageReader/__init__.py
+++ crafters/image/ImageReader/__init__.py
@@ -7,20 +7,24 @@ from jina.executors.crafters import BaseCrafter
 
 class ImageReader(BaseCrafter):
     """
-    :class:`ImageReader` loads the image from the given file path and save the `ndarray` of the image in the Document.
+    Load image file and craft it into image matrix.
+
+    :class:`ImageReader` loads the image from the given file
+        path and save the `ndarray` of the image in the Document.
+
+    :param channel_axis: the axis id of the color channel.
+        The ``-1`` indicates the color channel info at the last axis
     """
 
     def __init__(self, channel_axis: int = -1, *args, **kwargs):
-        """
-        :class:`ImageReader` load an image file and craft into image matrix.
-
-        :param channel_axis: the axis id of the color channel, ``-1`` indicates the color channel info at the last axis
-        """
+        """Set Constructor."""
         super().__init__(*args, **kwargs)
         self.channel_axis = channel_axis
 
     def craft(self, buffer: bytes, uri: str, *args, **kwargs) -> Dict:
         """
+        Read image file and craft it into image matrix.
+
         Read the image from the given file path that specified in `buffer` and save the `ndarray` of the image in
             the `blob` of the document.
 
