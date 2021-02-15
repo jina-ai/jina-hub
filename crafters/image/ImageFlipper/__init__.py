2ad94caa7bbe0a3760e5c4f8072f2bf8edb2259d
Susana Guzman <zuxi.w13@gmail.com>
2021-02-15 15:18:40 +0100

    docs: docstrings for ImageFlipper

--- crafters/image/ImageFlipper/__init__.py
+++ crafters/image/ImageFlipper/__init__.py
@@ -9,7 +9,16 @@ from .helper import _load_image, _move_channel_axis
 
 class ImageFlipper(BaseCrafter):
     """
-    :class:`ImageFlipper` flips the image horizontally or vertically. Flip image in the left/right or up/down direction respectively.
+    Flip the image horizontally or vertically.
+
+    Flip image in the left/right or up/down direction respectively.
+
+    :param vertical: desired rotation type.
+        ``True`` indicates the image should be flipped vertically.
+    :param channel_axis: the axis id of the color channel, ``-1``
+        indicates the color channel info at the last axis.
+    :param args:  Additional positional arguments
+    :param kwargs: Additional keyword arguments
     """
 
     def __init__(self,
@@ -17,11 +26,7 @@ class ImageFlipper(BaseCrafter):
                  channel_axis: int = -1,
                  *args,
                  **kwargs):
-        """
-
-        :param vertical: desired rotation type. ``True`` indicates the image should be flipped vertically.
-        :param channel_axis: the axis id of the color channel, ``-1`` indicates the color channel info at the last axis
-        """
+        """Set Constructor."""
         super().__init__(*args, **kwargs)
         self.vertical = vertical
         self.channel_axis = channel_axis
@@ -31,7 +36,7 @@ class ImageFlipper(BaseCrafter):
         Flip the input image array horizontally or vertically.
 
         :param blob: the ndarray of the image with the color channel at the last axis
-        :return: a chunk dict with the mage
+        :return: A dict with the flipped image
         """
         raw_img = _load_image(blob, self.channel_axis)
         img = np.array(raw_img).astype('float32')
