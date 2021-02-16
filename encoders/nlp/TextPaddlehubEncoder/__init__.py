901f11c51feddff2e5fb23e7bc0e1bac2bf7b16d
Susana Guzman <zuxi.w13@gmail.com>
2021-02-16 13:27:02 +0100

    docs: docstring for TextPaddleHubEncoder

--- encoders/nlp/TextPaddlehubEncoder/__init__.py
+++ encoders/nlp/TextPaddlehubEncoder/__init__.py
@@ -6,27 +6,27 @@ from jina.executors.decorators import batching, as_ndarray
 
 class TextPaddlehubEncoder(BasePaddleEncoder):
     """
-    :class:`TextPaddlehubEncoder` encodes data from an array of string in size `B` into a ndarray in size `B x D`.
+    Encode data from an array of string in size `B` into a ndarray in size `B x D`.
+
     Internally, :class:`TextPaddlehubEncoder` wraps the Ernie module from paddlehub.
     https://github.com/PaddlePaddle/PaddleHub
+    For models' details refer to
+    https://www.paddlepaddle.org.cn/hublist?filter=en_category&value=SemanticModel
+
+    :param model_name: the name of the model. Supported models include
+        ``ernie``, ``ernie_tiny``, ``ernie_v2_eng_base``, ``ernie_v2_eng_large``,
+        ``bert_chinese_L-12_H-768_A-12``, ``bert_multi_cased_L-12_H-768_A-12``,
+        ``bert_multi_uncased_L-12_H-768_A-12``, ``bert_uncased_L-12_H-768_A-12``,
+        ``bert_uncased_L-24_H-1024_A-16``, ``chinese-bert-wwm``,
+        ``chinese-bert-wwm-ext``, ``chinese-electra-base``,
+        ``chinese-electra-small``, ``chinese-roberta-wwm-ext``,
+        ``chinese-roberta-wwm-ext-large``, ``rbt3``, ``rbtl3``
+    :param args:  Additional positional arguments
+    :param kwargs: Additional keyword arguments
     """
 
     def __init__(self, model_name: str = None, *args, **kwargs):
-        """
-
-        :param model_name: the name of the model. Supported models include
-            ``ernie``, ``ernie_tiny``, ``ernie_v2_eng_base``, ``ernie_v2_eng_large``,
-            ``bert_chinese_L-12_H-768_A-12``, ``bert_multi_cased_L-12_H-768_A-12``,
-                ``bert_multi_uncased_L-12_H-768_A-12``, ``bert_uncased_L-12_H-768_A-12``,
-                ``bert_uncased_L-24_H-1024_A-16``,
-            ``chinese-bert-wwm``, ``chinese-bert-wwm-ext``,
-            ``chinese-electra-base``, ``chinese-electra-small``,
-            ``chinese-roberta-wwm-ext``, ``chinese-roberta-wwm-ext-large``,
-            ``rbt3``, ``rbtl3``
-
-        For models' details refer to
-            https://www.paddlepaddle.org.cn/hublist?filter=en_category&value=SemanticModel
-        """
+        """Set Constructor."""
         super().__init__(*args, **kwargs)
         self.model_name = model_name or 'ernie_tiny'
 
@@ -38,6 +38,7 @@ class TextPaddlehubEncoder(BasePaddleEncoder):
     @as_ndarray
     def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
         """
+        Encode data from an array of string in size `B` into a ndarray in size `B x D`.
 
         :param data: a 1d array of string type in size `B`
         :return: an ndarray in size `B x D`
