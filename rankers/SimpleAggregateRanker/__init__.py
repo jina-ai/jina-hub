c38c120e6d898cdebb63078e02fa3ddc66456359
Han Xiao <han.xiao@jina.ai>
2021-02-18 05:09:36 +0800

    fix(ranker): fix parameter in SimpleAggregateRanker ranker

--- rankers/SimpleAggregateRanker/__init__.py
+++ rankers/SimpleAggregateRanker/__init__.py
@@ -1,5 +1,6 @@
-from jina.executors.rankers import Chunk2DocRanker
 import numpy as np
+from jina.executors.rankers import Chunk2DocRanker
+
 
 class SimpleAggregateRanker(Chunk2DocRanker):
     """
@@ -10,8 +11,7 @@ class SimpleAggregateRanker(Chunk2DocRanker):
 
     :param: aggregate_function: defines the used aggregate function
         and can be one of: [min, max, mean, median, sum, prod]
-    :param: is_reverse_score: True if a large score of the matched
-        chunks mean a lower score of the matched doc,othewise: False
+    :param: inverse_score: plus-one inverse by 1/(1+score)
     :raises:
         ValueError: If `aggregate_function` is not any of the expected types
     :param args:  Additional positional arguments
@@ -20,10 +20,10 @@ class SimpleAggregateRanker(Chunk2DocRanker):
 
     AGGREGATE_FUNCTIONS = ['min', 'max', 'mean', 'median', 'sum', 'prod']
 
-    def __init__(self, aggregate_function: str, is_reversed_score: bool, *args, **kwargs):
+    def __init__(self, aggregate_function: str, inverse_score: bool = False, *args, **kwargs):
         """Set constructor"""
         super().__init__(*args, **kwargs)
-        self.is_reversed_score = is_reversed_score
+        self.inverse_score = inverse_score
         if aggregate_function in self.AGGREGATE_FUNCTIONS:
             self.np_aggregate_function = getattr(np, aggregate_function)
         else:
@@ -32,9 +32,9 @@ class SimpleAggregateRanker(Chunk2DocRanker):
     def _get_score(self, match_idx, query_chunk_meta, match_chunk_meta, *args, **kwargs):
         scores = match_idx[self.COL_SCORE]
         aggregated_score = self.np_aggregate_function(scores)
-        if self.is_reversed_score:
-            if aggregated_score == -1:
+        if self.inverse_score:
+            if aggregated_score < 0:
                 raise ValueError(f'Setting "is_reversed_score" to True does not allow to have an aggregated document '
-                                 f'score of -1.')
+                                 f'negative score')
             aggregated_score = 1. / (1. + aggregated_score)
         return self.get_doc_id(match_idx), aggregated_score
