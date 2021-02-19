86c71eb04e9bf344b028e7876b7a3a286057d9f3
Susana Guzman <zuxi.w13@gmail.com>
2021-02-19 11:08:03 +0100

    docs: docstring for RandomGaussianEncoder

--- encoders/numeric/RandomGaussianEncoder/__init__.py
+++ encoders/numeric/RandomGaussianEncoder/__init__.py
@@ -5,7 +5,13 @@ from jina.executors.encoders.numeric import TransformEncoder
 
 class RandomGaussianEncoder(TransformEncoder):
     """
-    :class:`RandomGaussianEncoder` encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`
+    Reduce dimensionality using Gaussian random projection.
+
+    Encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`
+    Where `B` is the batch's size and `T` and `D` are the dimensions pre (`T`)
+    and after (`D`) the encoding.
+
+    More info can be found here:
     https://scikit-learn.org/stable/modules/generated/sklearn.random_projection.GaussianRandomProjection.html
     """
 
