0ae769b2dd6f41d6a3e3a64b36f2da117ee308c4
CatStark <susana.guzman@jina.ai>
2021-02-22 13:47:42 +0100

    docs: docstring for encoders/numeric (#3968)
    * docs: docstring for CompressionVaeEncoder
    
    * docs: docstring for FastICAEncoder
    
    * docs: docstring for FeatureAgglomerationEncoder
    
    * docs: update copyright
    
    * docs: docstring for IncrementalPCAEncoder
    
    * docs: docstring for RandomGaussianEncoder
    
    * docs: docstring for RandomSparseEncoder
    
    * docs: docstring for TSNEEncoder
    
    * docs: add args and kwargs
    
    * docs: add docstring for post_init

--- encoders/numeric/FastICAEncoder/__init__.py
+++ encoders/numeric/FastICAEncoder/__init__.py
@@ -1,8 +1,23 @@
+__copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
 from jina.executors.encoders.numeric import TransformEncoder
 
 class FastICAEncoder(TransformEncoder):
     """
-    :class:`FastICAEncoder` encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
+    Encodes data using a fast algorithm for Independent Component Analysis (FastICA)
+
+    Encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
+    Where `B` is the batch's size and `T` and `D` are the dimensions pre (`T`)
+    and after (`D`) the encoding.
+
+    :param output_dim: Output size.
+    :param num_features: Number of input features. If ``num_features`` is None,
+        ``num_features`` is inferred from the data.
+    :param whiten: If whiten is false, the data is already considered to be whitened,
+        and no whitening is performed.
+    :param args:  Additional positional arguments
+    :param kwargs: Additional keyword arguments
 
     .. note::
         :class:`FastICAEncoder` must be trained before calling ``encode()``.
@@ -10,13 +25,6 @@ class FastICAEncoder(TransformEncoder):
 
     def __init__(self, num_features: int = None, whiten: bool = False,
                  max_iter: int = 200, *args, **kwargs):
-        """
-
-        :param output_dim: the output size.
-        :param num_features: the number of input features.  If ``num_features`` is None, then ``num_features`` is
-            inferred from the data
-        :param whiten: If whiten is false, the data is already considered to be whitened, and no whitening is performed.
-        """
         super().__init__(*args, **kwargs)
         self.whiten = whiten
         self.num_features = num_features
@@ -25,6 +33,7 @@ class FastICAEncoder(TransformEncoder):
         self.model = None
 
     def post_init(self):
+        """Load FastICA model"""
         super().post_init()
         if not self.model:
             from sklearn.decomposition import FastICA
@@ -32,4 +41,3 @@ class FastICAEncoder(TransformEncoder):
                 n_components=self.output_dim,
                 whiten=self.whiten,
                 max_iter=self.max_iter)
-
--- encoders/numeric/IncrementalPCAEncoder/__init__.py
+++ encoders/numeric/IncrementalPCAEncoder/__init__.py
@@ -1,25 +1,33 @@
-__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 from jina.executors.encoders.numeric import TransformEncoder
 
 class IncrementalPCAEncoder(TransformEncoder):
     """
-    :class:`IncrementalPCAEncoder` encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
+    Encodes data from an ndarray in size `B x T` into an ndarray in size `B x D`.
+
+    Where `B` is the batch's size and `T` and `D` are the dimensions pre (`T`)
+    and after (`D`) the encoding.
+
+    :param output_dim: the output size.
+    :param num_features: the number of input features.
+        If ``num_features`` is None, then ``num_features``
+        is inferred from the data
+    :param whiten: If whiten is false, the data is already considered to be whitened,
+        and no whitening is performed.
+    :param args:  Additional positional arguments
+    :param kwargs: Additional keyword arguments
+
+    More details can be found
+    `here <https://scikit-learn.org/stable/auto_examples/decomposition/plot_incremental_pca.html>`_
 
     .. note::
-        :class:`IncrementalPCAEncoder` must be trained before calling ``encode()``. This encoder can be trained in an
-        incremental way.
+        :class:`IncrementalPCAEncoder` must be trained before calling ``encode()``.
+        This encoder can be trained in an incremental way.
     """
 
     def __init__(self, num_features: int = None, whiten: bool = False, *args, **kwargs):
-        """
-
-        :param output_dim: the output size.
-        :param num_features: the number of input features.  If ``num_features`` is None, then ``num_features`` is
-            inferred from the data
-        :param whiten: If whiten is false, the data is already considered to be whitened, and no whitening is performed.
-        """
         super().__init__(*args, **kwargs)
         self.whiten = whiten
         self.num_features = num_features
@@ -27,6 +35,7 @@ class IncrementalPCAEncoder(TransformEncoder):
         self.model = None
 
     def post_init(self):
+        """Load IncrementalPCA model"""
         super().post_init()
         if not self.model:
             from sklearn.decomposition import IncrementalPCA
