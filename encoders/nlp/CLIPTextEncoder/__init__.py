17d7f1fae0fdfc491858eca4c667a217a8f4b68a
David Buchaca Prats <davidbuchaca@gmail.com>
2021-02-24 18:51:10 +0100

    docs: Docummentation added

--- encoders/nlp/CLIPTextEncoder/__init__.py
+++ encoders/nlp/CLIPTextEncoder/__init__.py
@@ -15,6 +15,8 @@ class CLIPTextEncoder(BaseTorchEncoder):
     a `np.ndarray` of shape `Batchsize x EmbeddingDimension`. 
 
     Internally, :class:`CLIPImageEncoder` wraps the `CLIP` model from https://github.com/openai/CLIP
+
+    :param model_name: the name of the model. Supported models include ``ViT-B/32`` and ``RN50``.
     """
     def __init__(self, model_name: str ='ViT-B/32',
                  *args, **kwargs):
@@ -22,9 +24,6 @@ class CLIPTextEncoder(BaseTorchEncoder):
         self.model_name = model_name
 
     def post_init(self):
-        """
-        :param model_name: the name of the model. Supported models include ``ViT-B/32`` and ``RN50``.
-        """
         import clip
         model, _ = clip.load(self.model_name, self.device)
         self.model = model
