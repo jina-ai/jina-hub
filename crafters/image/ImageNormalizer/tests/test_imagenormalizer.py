e3adb1bdc778327726a3da0dbd7fff1a6f754c3b
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-08 18:03:10 +0100

    feat: use single decorator for image crafters

--- crafters/image/ImageNormalizer/tests/test_imagenormalizer.py
+++ crafters/image/ImageNormalizer/tests/test_imagenormalizer.py
@@ -11,12 +11,16 @@ def test_transform_results():
     target_size = 224
     crafter = ImageNormalizer(target_size=target_size)
     img_array = create_random_img_array(img_size, img_size)
-    crafted_doc = crafter.craft(img_array)
-    assert crafted_doc["blob"].shape == (224, 224, 3)
+    crafted_docs = crafter.craft([img_array, img_array])
+    assert len(crafted_docs) == 2
+    for crafted_doc in crafted_docs:
+        assert crafted_doc["blob"].shape == (224, 224, 3)
 
     img_size = (217, 200)
     target_size = (100, 224)
     crafter = ImageNormalizer(target_size=target_size)
     img_array = create_random_img_array(*img_size)
-    crafted_doc = crafter.craft(img_array)
-    assert crafted_doc["blob"].shape[:-1] == target_size
+    crafted_docs = crafter.craft([img_array, img_array])
+    assert len(crafted_docs) == 2
+    for crafted_doc in crafted_docs:
+        assert crafted_doc["blob"].shape[:-1] == target_size
