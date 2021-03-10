4f1b4b9bf1a01243660e75b32463a3f95f654fa5
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-03-09 23:51:51 -0500

    fix(ranker): remove leven changes

--- rankers/LevenshteinRanker/tests/test_levenshteinranker.py
+++ rankers/LevenshteinRanker/tests/test_levenshteinranker.py
@@ -1,6 +1,12 @@
 __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
+import copy
+import json
+import numpy as np
+
+from jina.executors.rankers import Match2DocRanker
+
 from .. import LevenshteinRanker
 
 
@@ -8,7 +14,6 @@ def test_levenshteinranker():
     query_meta = {"text": "cool stuff"}
     old_match_scores = [5, 4]
     match_meta = [{"text": "cool stuff"}, {"text": "kewl stuff"}]
-
     ranker = LevenshteinRanker()
     new_scores = ranker.score(
         old_match_scores,
@@ -16,6 +21,4 @@ def test_levenshteinranker():
         match_meta
     )
 
-    assert len(new_scores) == 2
-    assert new_scores[0] == 0
-    assert new_scores[1] == -3
+    assert len(new_scores) == 1
