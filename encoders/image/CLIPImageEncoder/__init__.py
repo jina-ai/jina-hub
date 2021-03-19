787ca22f511a9dd52cde13450954791fb5c758b4
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-03-19 08:21:03 +0100

    fix: fix torch import in clip image encoder

--- encoders/image/CLIPImageEncoder/__init__.py
+++ encoders/image/CLIPImageEncoder/__init__.py
@@ -2,11 +2,11 @@ __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
 import numpy as np
-import torch
 
 from jina.executors.decorators import batching, as_ndarray
 from jina.executors.encoders.frameworks import BaseTorchEncoder
 from jina.executors.devices import TorchDevice
+
 
 class CLIPImageEncoder(BaseTorchEncoder):
     """Encode data from a numpy `ndarray` of shape `BatchSize x (Channel x Height x Width)` into
@@ -18,7 +18,8 @@ class CLIPImageEncoder(BaseTorchEncoder):
     :param args: Additional positional arguments.
     :param kwargs: Additional positional arguments.
     """
-    def __init__(self, model_name: str ='ViT-B/32',
+
+    def __init__(self, model_name: str = 'ViT-B/32',
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model_name = model_name
@@ -27,7 +28,7 @@ class CLIPImageEncoder(BaseTorchEncoder):
         """Load a model from clip specified in `model_name`."""
         import clip
 
-        assert self.model_name in clip.available_models(),\
+        assert self.model_name in clip.available_models(), \
             f'model_name={self.model_name} not in clip.available_models'
 
         model, _ = clip.load(self.model_name, self.device)
@@ -44,13 +45,14 @@ class CLIPImageEncoder(BaseTorchEncoder):
         :param kwargs: Additional positional arguments.
         :return: A `BatchSize x EmbeddingDimension` numpy array.
         """
+        import torch
+
         input_torchtensor = torch.from_numpy(data.astype('float32'))
         if self.on_gpu:
             input_torchtensor = input_torchtensor.cuda()
 
         with torch.no_grad():
-            embedded_data = self.model.encode_image(input_torchtensor)   
+            embedded_data = self.model.encode_image(input_torchtensor)
 
         embedded_data = embedded_data.cpu().numpy()
         return embedded_data
-    
