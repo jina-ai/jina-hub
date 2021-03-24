831b91c3a854ba893511f4e255892410ac96c56b
Anton Kiselev <akiselev@provectus.com>
2021-03-24 12:27:34 +0300

    feat(facenetsegmenter): review for tests

--- segmenters/image/FaceNetSegmenter/tests/test_facenetsegmenter.py
+++ segmenters/image/FaceNetSegmenter/tests/test_facenetsegmenter.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -16,6 +17,13 @@ def segmenter():
     return FaceNetSegmenter()
 
 
+def assert_correct_output(result, n_faces: int):
+    assert isinstance(result, list)
+    assert len(result) == n_faces
+    for face in result:
+        assert face['blob'].shape == (3, 160, 160)
+
+
 @pytest.mark.parametrize('filename', ['one_face.jpg',
                                       'three_faces.jpg',
                                       'four_faces.jpg',
@@ -25,9 +33,7 @@ def test_segment_face(segmenter, filename):
 
     result = segmenter.segment(np.array(image))
 
-    assert isinstance(result, list)
-    assert len(result) == 1
-    assert result[0]['blob'].shape == (3, 160, 160)
+    assert_correct_output(result, n_faces=1)
 
 
 @pytest.mark.parametrize('height', [128, 512])
@@ -37,8 +43,7 @@ def test_segment_no_face(segmenter, height: int, width: int):
 
     result = segmenter.segment(image)
 
-    assert isinstance(result, list)
-    assert len(result) == 0
+    assert_correct_output(result, n_faces=0)
 
 
 @pytest.fixture
@@ -46,21 +51,17 @@ def segmenter_multiface():
     return FaceNetSegmenter(keep_all=True, post_process=False)
 
 
-@pytest.mark.parametrize('args', [
+@pytest.mark.parametrize('filename, n_faces', [
     ('three_faces.jpg', 3),
     ('four_faces.jpg', 4),
     ('five_faces.jpg', 5)
 ])
-def test_segment_face(segmenter_multiface, args):
-    filename, n_faces = args
-    image = Image.open(SEGMENTER_DIR / 'imgs' / filename)
+def test_segment_face(segmenter_multiface, filename, n_faces):
+    image = Image.open(os.path.join(SEGMENTER_DIR, 'imgs', filename))
 
     result = segmenter_multiface.segment(np.array(image))
 
-    assert isinstance(result, list)
-    assert len(result) == n_faces
-    for face in result:
-        assert face['blob'].shape == (3, 160, 160)
+    assert_correct_output(result, n_faces=n_faces)
 
 
 if __name__ == '__main__':
