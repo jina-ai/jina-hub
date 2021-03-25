956ad6b1866c8a317c7bd981262c79b59db28e5a
Susana Guzman <zuxi.w13@gmail.com>
2021-03-25 10:10:48 +0100

    feat: set database connection

--- indexers/keyvalue/PostgreSQLDBIndexer/__init__.py
+++ indexers/keyvalue/PostgreSQLDBIndexer/__init__.py
@@ -3,25 +3,16 @@ __license__ = "Apache-2.0"
 
 from typing import Optional, Iterable
 
-from jina.executors.indexers.keyvalue import BinaryPbIndexer
+from jina.executors.indexers import BaseIndexer
 from jina.helper import cached_property
 
 if False:
     from ..PostgreSQLDBIndexer.postgresqldbhandler import PostgreSQLDBHandler
 
 
-class PostgreSQLDBIndexer(BinaryPbIndexer):
-    """:class:`PostgreSQLDBIndexer` PostgreSQL based KV Indexer."""
+class PostgreSQLDBIndexer(BaseIndexer):
+    """:class:`PostgreSQLDBIndexer` PostgreSQL based KV Indexer.
 
-    # TODO user name and password as environment variables
-    def __init__(self,
-                 hostname: str = '127.0.0.1',
-                 port: int = 5432,
-                 username: Optional[str] = None,
-                 password: Optional[str] = None,
-                 database: str = 'postgres',
-                 *args, **kwargs):
-        """
         Initialize the PostgreSQLDBIndexer.
 
         :param hostname: hostname of the machine
@@ -32,7 +23,17 @@ class PostgreSQLDBIndexer(BinaryPbIndexer):
         :param collection: the collection name
         :param args: other arguments
         :param kwargs: other keyword arguments
-        """
+    """
+
+
+    def __init__(self,
+                 hostname: str = '127.0.0.1',
+                 port: int = 5432,
+                 username: str = 'default_name',
+                 password: str = 'default_name',
+                 database: str = 'default_db',
+                 *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.hostname = hostname
         self.port = port
@@ -40,73 +41,46 @@ class PostgreSQLDBIndexer(BinaryPbIndexer):
         self.password = password
         self.database_name = database
 
-    def post_init(self):
-        """Initialize the PostgreSQLDBHandler inside the Indexer."""
-        from .postgresqldbhandler import PostgreSQLDBHandler
-        super().post_init()
-        self.handler = PostgreSQLDBHandler(hostname=self.hostname,
-                                           port=self.port,
-                                           username=self.username,
-                                           password=self.password,
-                                           database=self.database_name)
+    def __enter__(self):
+        return self.connect()
 
-    def get_add_handler(self) -> 'PostgreSQLDBHandler':
-        """Get the handler to PostgreSQL."""
-        return self.handler
+    def connect(self):
+        """Connect to the database. """
 
-    def get_create_handler(self) -> 'PostgreSQLDBHandler':
-        """Get the handler to PostgreSQL."""
-        return self.handler
+        import psycopg2
+        from psycopg2 import Error, extras
 
-    def get_query_handler(self) -> 'PostgreSQLDBHandler':
-        """Get the handler to PostgreSQL."""
-        return self.handler
+        try:
+            self.connection = psycopg2.connect(user=self.username,
+                password=self.password,
+                database=self.database_name,
+                host=self.hostname,
+                port=self.port)
+            self.cursor = self.connection.cursor()
+            self.logger.info('Successfully connected to the database')
+        except (Exception, Error) as error:
+            print('Error while connecting to PostgreSQL', error)
+        return self
 
-    def add(self, keys: Iterable[str], values: Iterable[bytes], *args, **kwargs) -> None:
-        """Add a Document to PostgreSQL.
+    def add(self, ids, vecs, metas, *args, **kwargs):
+        raise NotImplementedError
 
-        :param keys: the keys by which you will add
-        :param values: the values
-        :param args: other args
-        :param kwargs: other kwargs
+    def update(self, ids, vecs, metas, *args, **kwargs):
+        raise NotImplementedError
+
+    def delete(self, ids, *args, **kwargs):
+        raise NotImplementedError
+
+    def dump(self, uri, shards, formats):
+        raise NotImplementedError
+
+    def __exit__(self, *args):
+        """ Make sure the connection to the database is closed.
         """
-        with self.write_handler as PostgreSQL_handler:
-            for i, j in zip(keys, values):
-                doc = {'_id': i, 'values': j}
-                inserted_ids = PostgreSQL_handler.add(documents=[doc])
-                if len(inserted_ids) != 1:
-                    raise Exception(f'Mismatch in PostgreSQL insert')
-
-    @cached_property
-    def query_handler(self) -> 'PostgreSQL_handler':
-        """Get the handler to PostgreSQL."""
-        return self.get_query_handler()
-
-    def query(self, key: str, *args, **kwargs) -> Optional[bytes]:
-        """Query the serialized documents by document id.
-
-        :param key: document id
-        :return: serialized document
-        """
-        with self.query_handler as PostgreSQL_handler:
-            result = PostgreSQL_handler.query(key)
-
-        if result:
-            return result
-
-    def update(self, keys: Iterable[str], values: Iterable[bytes], *args, **kwargs) -> None:
-        """Update the document at the given key in the database.
-
-        :param keys: document ids to update
-        :param values: serialized documents
-        """
-        with self.query_handler as PostgreSQL_handler:
-            PostgreSQL_handler.update(keys, values)
-
-    def delete(self, keys: Iterable[str], *args, **kwargs) -> None:
-        """Delete documents from the indexer.
-
-        :param keys: document ids to delete
-        """
-        with self.query_handler as PostgreSQL_handler:
-            PostgreSQL_handler.delete(keys)
+        from psycopg2 import Error
+        try:
+            self.connection.close()
+            self.cursor.close()
+            print('PostgreSQL connection is closed')
+        except (Exception, Error) as error:
+            print('Error while closing: ', error)
