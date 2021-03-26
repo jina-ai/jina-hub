f9e0095da46174d0f27e37423a5b5db59c6514e7
Susana Guzman <zuxi.w13@gmail.com>
2021-03-26 09:47:33 +0100

    feat: add test

--- indexers/keyvalue/PostgreSQLDBIndexer/tests/test_PostgreSQLdbindexer.py
+++ indexers/keyvalue/PostgreSQLDBIndexer/tests/test_PostgreSQLdbindexer.py
@@ -1,77 +1,31 @@
-import random
-from collections import Iterable
+import os
+import shutil
+import sys
+from typing import Iterator
 
 import numpy as np
+import pytest
+
 from jina import Document
+from .. import PostgreSQLDBIndexer
 
-from .. import MongoDBIndexer
+from tests import get_documents
 
+def doc_without_embedding(d):
+    new_doc = Document()
+    new_doc.CopyFrom(d)
+    new_doc.ClearField('embedding')
+    return new_doc
 
-def _random_docs(num_docs, chunks_per_doc=5, embed_dim=10, jitter=1):
-    c_id = 3 * num_docs  # avoid collision with docs
-    for j in range(num_docs):
-        with Document() as d:
-            d.tags['id'] = j
-            d.text = b'hello world doc id %d' % j
-            d.embedding = np.random.random([embed_dim + np.random.randint(0, jitter)])
-            for k in range(chunks_per_doc):
-                with Document() as c:
-                    c.text = 'i\'m chunk %d from doc %d' % (c_id, j)
-                    c.embedding = np.random.random([embed_dim + np.random.randint(0, jitter)])
-                    c.tags['id'] = c_id
-                    c.tags['parent_id'] = j
-                    c_id += 1
-                    c.parent_id = d.id
-                d.chunks.add(c)
-        yield d
-
-
-def test_mongodbindexer():
-    num_docs = 5
-    docs = list(_random_docs(num_docs=num_docs,
-                            chunks_per_doc=3))
-    keys: Iterable[str] = [doc.id for doc in docs]
-    values = [doc.SerializeToString() for doc in docs]
-
-    query_index = random.randint(0, num_docs - 1)
-    # adding type annotations to remove further warnings
-    query_key: str = docs[query_index].id
-    query_text = docs[query_index].text
-
-    # add
-    with MongoDBIndexer() as mongo_indexer:
-        mongo_indexer.add(keys=keys, values=values)
-
-    with MongoDBIndexer() as mongo_query:
-        result = mongo_query.query(key=query_key)
-        assert result['_id'] == query_key
-        d = Document()
-        d.ParseFromString(result['values'])
-        assert d.text == query_text
-
-    # documents to be updated
-    new_docs = list(_random_docs(num_docs=num_docs, chunks_per_doc=3))
-    # documents for insertion
-    new_values = [doc.SerializeToString() for doc in new_docs]
-    # documents for assertion
-    new_texts = [doc.text for doc in new_docs]
-
-    with MongoDBIndexer() as mongo_indexer:
-        mongo_indexer.update(keys=keys, values=new_values)
-
-    with MongoDBIndexer() as mongo_query:
-        for key, new_value in zip(keys, new_texts):
-            result = mongo_query.query(key)
-            assert result['_id'] == key
-            new_doc = Document()
-            new_doc.ParseFromString(result['values'])
-            assert new_value == new_doc.text
-
-    # delete
-    with MongoDBIndexer() as mongo_query:
-        mongo_query.delete(keys)
-
-    with MongoDBIndexer() as mongo_query:
-        for key in keys:
-            result = mongo_query.query(key)
-            assert result is None
+def test_postgress():
+    with PostgreSQLDBIndexer(username='susana', password='pwd', database='python_test',
+            table='sql') as postgres_indexer:
+        #postgres_indexer.add(8, 23, 'test')
+        docs = list(get_documents(chunks=0, same_content=False))
+        info = [
+            (doc.id, doc.embedding, doc_without_embedding(doc).SerializeToString())
+            for doc in docs
+        ]
+        if info:
+            ids, vecs, metas = zip(*info)
+            postgres_indexer.add(ids, vecs, metas)
