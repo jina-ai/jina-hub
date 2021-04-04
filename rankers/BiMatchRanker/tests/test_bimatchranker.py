13407425071e6665987dcc7c8c84b2ef9e61a31c
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-04-03 21:03:12 -0400

    fix(ranker): change length to siblings

--- rankers/BiMatchRanker/tests/test_bimatchranker.py
+++ rankers/BiMatchRanker/tests/test_bimatchranker.py
@@ -9,18 +9,18 @@ def test_bimatchranker():
     Before grouping:
     query_chunk2match_chunk = {
         100: [
-            {'parent_id': 1, 'id': 10, 'score': 0.4, 'length': 200},
+            {'parent_id': 1, 'id': 10, 'score': 0.4, 'siblings': 200},
         ],
         110: [
-            {'parent_id': 1, 'id': 10, 'score': 0.3, 'length': 200},
-            {'parent_id': 1, 'id': 11, 'score': 0.2, 'length': 200},
-            {'parent_id': 4294967294, 'id': 20, 'score': 0.1, 'length': 300},
+            {'parent_id': 1, 'id': 10, 'score': 0.3, 'siblings': 200},
+            {'parent_id': 1, 'id': 11, 'score': 0.2, 'siblings': 200},
+            {'parent_id': 4294967294, 'id': 20, 'score': 0.1, 'siblings': 300},
         ]
     }
     """
     ranker = BiMatchRanker()
-    query_chunk_meta = {'100': {'length': 2}, '110': {'length': 2}}
-    match_chunk_meta = {'10': {'length': 200}, '11': {'length': 200}, '20': {'length': 300}}
+    query_chunk_meta = {'100': {'siblings': 2}, '110': {'siblings': 2}}
+    match_chunk_meta = {'10': {'siblings': 200}, '11': {'siblings': 200}, '20': {'siblings': 300}}
 
     # test score() with fake grouped data
     match_idx_1 = np.array([('1', '10', '100', 0.4), ('1', '10', '110', 0.3),
@@ -41,25 +41,25 @@ def test_bimatchranker_readme():
     """
     query_chunk2match_chunk = {
     1: [
-        {'parent_id': 1, 'id': 11, 'score': 0.1, 'length': 4},
-        {'parent_id': 2, 'id': 22, 'score': 0.5, 'length': 3},
-        {'parent_id': 2, 'id': 21, 'score': 0.7, 'length': 3},
+        {'parent_id': 1, 'id': 11, 'score': 0.1, 'siblings': 4},
+        {'parent_id': 2, 'id': 22, 'score': 0.5, 'siblings': 3},
+        {'parent_id': 2, 'id': 21, 'score': 0.7, 'siblings': 3},
     ],
     2: [
-        {'parent_id': 2, 'id': 21, 'score': 0.1, 'length': 3},
-        {'parent_id': 1, 'id': 11, 'score': 0.5, 'length': 4},
-        {'parent_id': 2, 'id': 22, 'score': 0.7, 'length': 3},
+        {'parent_id': 2, 'id': 21, 'score': 0.1, 'siblings': 3},
+        {'parent_id': 1, 'id': 11, 'score': 0.5, 'siblings': 4},
+        {'parent_id': 2, 'id': 22, 'score': 0.7, 'siblings': 3},
     ],
     3: [
-        {'parent_id': 2, 'id': 21, 'score': 0.1, 'length': 3},
-        {'parent_id': 2, 'id': 22, 'score': 0.5, 'length': 3},
-        {'parent_id': 2, 'id': 23, 'score': 0.7, 'length': 3},
+        {'parent_id': 2, 'id': 21, 'score': 0.1, 'siblings': 3},
+        {'parent_id': 2, 'id': 22, 'score': 0.5, 'siblings': 3},
+        {'parent_id': 2, 'id': 23, 'score': 0.7, 'siblings': 3},
     ]
     }
     """
     ranker = BiMatchRanker(d_miss=1)
-    query_chunk_meta = {'1': {'length': 3}, '2': {'length': 3}, '3': {'length': 3}}
-    match_chunk_meta = {'11': {'length': 4}, '22': {'length': 3}, '21': {'length': 3}, '23': {'length': 3}}
+    query_chunk_meta = {'1': {'siblings': 3}, '2': {'siblings': 3}, '3': {'siblings': 3}}
+    match_chunk_meta = {'11': {'siblings': 4}, '22': {'siblings': 3}, '21': {'siblings': 3}, '23': {'siblings': 3}}
     # test score() with fake grouped data
 
     match_idx_1 = np.array([('1', '11', '1', 0.1), ('1', '11', '2', 0.5)],
