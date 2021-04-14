cce2d4ed869d5861b458505e6d7b76d136e12ca3
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2021-04-14 18:09:18 +0800

    feat: add pydantic script

--- scripts/pydanticJSONgenerator.py
+++ scripts/pydanticJSONgenerator.py
@@ -1,7 +1,9 @@
+import enum
 from enum import Enum
 from typing import List
 from pydantic import BaseModel
 from pydantic.fields import Field
+import pyperclip
 
 class Types(str, Enum):
     pod = 'pod'
@@ -16,6 +18,9 @@ class Kinds(str, Enum):
     classifier = "classifier"
     evaluator = "evaluator"
 
+License = enum.Enum('License', {'apache-2.0': ' Apache license 2.0', 'afl-3.0': ' Academic Free License v3.0', 'artistic-2.0': ' Artistic license 2.0', 'bsl-1.0': ' Boost Software License 1.0', 'bsd-2-clause': ' BSD 2-clause "Simplified" license', 'bsd-3-clause': ' BSD 3-clause "New" or "Revised" license', 'bsd-3-clause-clear': ' BSD 3-clause Clear license', 'cc': ' Creative Commons license family', 'cc0-1.0': ' Creative Commons Zero v1.0 Universal', 'cc-by-4.0': ' Creative Commons Attribution 4.0', 'cc-by-sa-4.0': ' Creative Commons Attribution Share Alike 4.0', 'wtfpl': ' Do What The F*ck You Want To Public License', 'ecl-2.0': ' Educational Community License v2.0', 'epl-1.0': ' Eclipse Public License 1.0', 'eupl-1.1': ' European Union Public License 1.1', 'agpl-3.0': ' GNU Affero General Public License v3.0', 'gpl': ' GNU General Public License family', 'gpl-2.0': ' GNU General Public License v2.0', 'gpl-3.0': ' GNU General Public License v3.0', 'lgpl': ' GNU Lesser General Public License family', 'lgpl-2.1': ' GNU Lesser General Public License v2.1', 'lgpl-3.0': ' GNU Lesser General Public License v3.0', 'isc': ' ISC', 'lppl-1.3c': ' LaTeX Project Public License v1.3c', 'ms-pl': ' Microsoft Public License', 'mit': ' MIT', 'mpl-2.0': ' Mozilla Public License 2.0', 'osl-3.0': ' Open Software License 3.0', 'postgresql': ' PostgreSQL License', 'ofl-1.1': ' SIL Open Font License 1.1', 'ncsa': ' University of Illinois/NCSA Open Source License', 'unlicense': ' The Unlicense', 'zlib': ' zLib License'})
+
+
 class JinaManifestSchema(BaseModel):
     id : str = "https://api.jina.ai/schemas/hub/1.1.2.json"
     name: str = Field(default=...,
@@ -26,10 +31,12 @@ class JinaManifestSchema(BaseModel):
                         title='type',
                         additionalProperties=False,
                         description='The type of the image')
+
     kind: Kinds = Field(default=Kinds.encoder,
                         title='kind',
                         additionalProperties=False,
-                        description='The kind of the executor')
+                        description="The kind of the executor")
+
     author: str = Field(default='Jina AI Dev-Team (dev-team@jina.ai)',
                         description='Contact details of the people or organization responsible for the image (string)')
     version: int = Field(default=1,
@@ -42,13 +49,13 @@ class JinaManifestSchema(BaseModel):
                      description="URL to get documentation on the image (string)")
     vendor : str = Field(default="Jina AI Limited",
                      description="The name of the distributing entity, organization or individual (string)")
-    license : str = Field(default="apache-2.0",
-                     description="License under which contained software is distributed")
+    license : License = Field(default="apache-2.0",
+                                  description="License under which contained software is distributed")
     platform : str = Field(default="linux/amd64",
                      description="A list of CPU architectures that your image built on")
     keywords : List = Field(default=[],
                             title='keywords',
                      description="A list of strings help user to filter and locate your package")
 
-
-print(JinaManifestSchema(name='abc', description='def').schema_json(indent=2))
+pyperclip.copy(JinaManifestSchema(name='abc', description='def').schema_json(indent=2))
+#print(JinaManifestSchema(name='abc', description='def').schema_json(indent=2))
