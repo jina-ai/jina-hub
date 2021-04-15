f8a97452cbf4c3da5660b1516bf65bfb0c5a77a8
yongxuanzhang <yongxuanzhang93@gmail.com>
2021-04-15 17:08:01 -0400

    feat: pdf segmenter draft

---
+++ segmenters/nlp/PDFPlumberSegmenter/README.md
@@ -0,0 +1,46 @@
+# PDFPlumberSegmenter
+
+## 🗝️ Key Concepts
+This is a simple flow test where you can use the PDFExtractorSegmenter.
+This Segmenter will create a chunk per blob.
+
+So if we have a PDF with 2 images and text, we will have 3 chunks; one per each image and one per the text.
+
+
+## 🏃️ Flow
+First you define your flow here:
+    
+    f = Flow().add(uses='PDFExtractorSegmenter', array_in_pb=True)
+
+If you have a PDF with images and text, the ```array_in_pb=True``` is necessary.
+
+And then we search in: 
+
+    with f:
+        f.search(input_fn=search_generator(path=path, buffer=None), on_done=validate_mix_fn)
+
+For this example I have 3 different PDF files to test. 
+
+ - **Image only:** If we want to test the only-images PDF file, we use  ```validate_img_fn``` in the ```on_done```
+ - **Text only:** If we want to test the only-text PDF file, we use  ```validate_text_fn``` in the ```on_done```
+ - **Image and text:** If we want to test the mixed PDF file (images and text), we use  ```validate_mix_fn``` in the ```on_done```
+ 
+ ## 🤔  Tests
+ 
+For this examples we have 2 images per pdf, one of size 1024 x 660, and the other of 1191 x 626.
+
+ #### 🧪 Images
+  
+For the image validations we will check each chunk and compare it with the original image
+
+    assert blob.shape[1], blob.shape[0] == img.size
+
+ #### 🧪 Text
+And for the text we will check that that chunk corresponds with the expected text
+
+```
+expected_text = A cat poem I love cats, I love every kind of cat, I just wanna hug all of them, but I can't,
+I'm thinking about cats again I think about how cute they are\nAnd their whiskers and their nose"
+
+assert expected_text == d.chunks[2].text
+```
---
+++ segmenters/nlp/PDFPlumberSegmenter/tests/test_pdfextractorcrafter.py
@@ -0,0 +1,90 @@
+import os
+import pytest
+
+import numpy as np
+from PIL import Image
+
+from .. import PDFPlumberSegmenter
+
+expected_text = "A cat poem\nI love cats, I love every kind of cat,\nI just wanna hug all of them, but I can't," \
+                "\nI'm thinking about cats again\nI think about how cute they are\nAnd their whiskers and their " \
+                "nose"
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+path_img_text = os.path.join(cur_dir, 'cats_are_awesome.pdf')
+path_text = os.path.join(cur_dir, 'cats_are_awesome_text.pdf')
+path_img = os.path.join(cur_dir, 'cats_are_awesome_img.pdf')
+
+with open(path_text, 'rb') as pdf:
+    input_bytes_text = pdf.read()
+
+with open(path_img, 'rb') as pdf:
+    input_bytes_image = open(path_img, 'rb').read()
+
+with open(path_img_text, 'rb') as pdf:
+    input_bytes_images_text = pdf.read()
+
+
+@pytest.mark.parametrize('inputs', [
+    [np.stack([path_img_text, path_img_text]), [None, None], ['application/pdf', 'application/pdf']],
+    [[None, None], [input_bytes_images_text, input_bytes_images_text], ['application/pdf', 'application/pdf']],
+])
+def test_io_images_and_text(inputs):
+    segmenter = PDFPlumberSegmenter()
+    docs_chunks = segmenter.segment(*inputs)
+    assert len(docs_chunks) == 2
+    for chunks in docs_chunks:
+
+        assert len(chunks) == 3
+
+        # Check images
+        for idx, c in enumerate(chunks[:-1]):
+            with Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg')) as img:
+                blob = chunks[idx]['blob']
+                assert chunks[idx]['mime_type'] == 'image/png'
+                assert blob.shape[1], blob.shape[0] == img.size
+                if idx == 0:
+                    assert blob.shape == (660, 1024, 3)
+                if idx == 1:
+                    assert blob.shape == (626, 1191, 3)
+
+        # Check text
+        assert chunks[2]['text'] == expected_text
+        assert chunks[2]['mime_type'] == 'text/plain'
+
+
+@pytest.mark.parametrize('inputs', [
+    [np.stack([path_text, path_text]), [None, None], ['application/pdf', 'application/pdf']],
+    [[None, None], [input_bytes_text, input_bytes_text], ['application/pdf', 'application/pdf']],
+])
+def test_io_text(inputs):
+    segmenter = PDFPlumberSegmenter()
+    docs_chunks = segmenter.segment(*inputs)
+    assert len(docs_chunks) == 2
+    for chunks in docs_chunks:
+        assert len(chunks) == 1
+        # Check test
+        assert chunks[0]['text'] == expected_text
+        assert chunks[0]['mime_type'] == 'text/plain'
+
+
+@pytest.mark.parametrize('inputs', [
+    [np.stack([path_img, path_img]), [None, None], ['application/pdf', 'application/pdf']],
+    [[None, None], [input_bytes_image, input_bytes_image], ['application/pdf', 'application/pdf']],
+])
+def test_io_img(inputs):
+    segmenter = PDFPlumberSegmenter()
+    docs_chunks = segmenter.segment(*inputs)
+    assert len(docs_chunks) == 2
+    for chunks in docs_chunks:
+        assert len(chunks) == 2
+        # Check images
+        for idx, c in enumerate(chunks):
+            with Image.open(os.path.join(cur_dir, f'test_img_{idx}.jpg')) as im:
+                blob = chunks[idx]['blob']
+                assert chunks[idx]['mime_type'] == 'image/png'
+                assert blob.shape[1], blob.shape[0] == img.size
+                if idx == 0:
+                    assert blob.shape == (660, 1024, 3)
+                if idx == 1:
+                    assert blob.shape == (626, 1191, 3)
