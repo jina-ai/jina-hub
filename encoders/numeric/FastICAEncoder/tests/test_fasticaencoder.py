7bd442a4c0b2fc3a98aa2f5265aa3db5ae34b71a
Joan Fontanals <joan.martinez@jina.ai>
2021-04-16 17:08:47 +0200

    fix: fix fastica cannot train (#7586)

--- encoders/numeric/FastICAEncoder/tests/test_fasticaencoder.py
+++ encoders/numeric/FastICAEncoder/tests/test_fasticaencoder.py
@@ -1,84 +1,79 @@
-import numpy as np
 import os
 import pickle
-import shutil
 
-from jina.executors.encoders.numeric import TransformEncoder
+import numpy as np
+import pytest
+from sklearn.decomposition import FastICA
+
 from jina.executors import BaseExecutor
+from jina.executors.metas import get_default_metas
+
 from .. import FastICAEncoder
 
-input_dim = 28
-target_output_dim = 2
-train_data = np.random.rand(2000, input_dim)
 
-def rm_files(tmp_files):
-    for file in tmp_files:
-        if file and os.path.exists(file):
-            if os.path.isfile(file):
-                os.remove(file)
-            elif os.path.isdir(file):
-                shutil.rmtree(file, ignore_errors=False, onerror=None)
+@pytest.fixture(scope="function", autouse=True)
+def metas(tmpdir):
+    metas = get_default_metas()
+    metas['workspace'] = str(tmpdir)
+    yield metas
 
 
-def test_FastICATestCaseTrainCase():
-    requires_train_after_load = True
-    encoder = FastICAEncoder(
-        output_dim=target_output_dim, whiten=True, num_features=input_dim, max_iter=200)
-    encoder.train(train_data)
-    encoding_results(encoder)
-    save_and_load(encoder, requires_train_after_load)
-    save_and_load_config(encoder, requires_train_after_load)
-    rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
+@pytest.fixture(scope="function")
+def train_data():
+    """
+    Create an array of the given shape and populate it with random samples from a uniform distribution over [0, 1).
+    :return: a `B x T` numpy ``ndarray``, `B` is the size of the batch
+    """
+    batch_size = 2000
+    input_dim = 28
+    train_data = np.random.rand(batch_size, input_dim)
 
-def test_FastICATestCaseLoadCase():
-    requires_train_after_load = False
-    encoder = FastICAEncoder(
-        output_dim=target_output_dim, whiten=True, num_features=input_dim, max_iter=200)
-    encoder.train(train_data)
-    filename = 'ica_model.model'
-    pickle.dump(encoder.model, open(filename, 'wb'))
-    encoder = TransformEncoder(model_path=filename)
-    encoding_results(encoder)
-    save_and_load(encoder, requires_train_after_load)
-    save_and_load_config(encoder, requires_train_after_load)
-    rm_files([encoder.save_abspath, encoder.config_abspath, encoder.model_path])
+    return train_data
 
 
-def encoding_results(encoder):
-    test_data = np.random.rand(10, input_dim)
+@pytest.fixture(scope="function")
+def test_data():
+    """
+    Create an array of the given shape and populate it with random samples from a uniform distribution over [0, 1).
+    :return: a `B x T` numpy ``ndarray``, `B` is the size of the batch
+    """
+    batch_size = 10
+    input_dim = 28
+    test_data = np.random.rand(batch_size, input_dim)
+
+    return test_data
+
+
+def get_encoder(metas, train_data, target_output_dim):
+    tmpdir = metas['workspace']
+    model_path = os.path.join(tmpdir, 'fastica_model.model')
+
+    model = FastICA(n_components=target_output_dim,
+                    whiten=True,
+                    max_iter=10)
+    model.fit(train_data)
+    pickle.dump(model, open(model_path, 'wb'))
+
+    return FastICAEncoder(model_path=model_path)
+
+
+@pytest.mark.parametrize('target_output_dim', [2])
+def test_encoding_results(metas, train_data, test_data, target_output_dim):
+    expected_batch_size = test_data.shape[0]
+    encoder = get_encoder(metas, train_data, target_output_dim)
     encoded_data = encoder.encode(test_data)
-    assert encoded_data.shape == (test_data.shape[0], target_output_dim)
+    assert encoded_data.shape == (expected_batch_size, target_output_dim)
     assert type(encoded_data) is np.ndarray
 
 
-
-def save_and_load(encoder, requires_train_after_load):
-    test_data = np.random.rand(10, input_dim)
+@pytest.mark.parametrize('target_output_dim', [2])
+def test_save_and_load(metas, train_data, test_data, target_output_dim):
+    encoder = get_encoder(metas, train_data, target_output_dim)
     encoded_data_control = encoder.encode(test_data)
     encoder.touch()
     encoder.save()
     assert os.path.exists(encoder.save_abspath)
+
     encoder_loaded = BaseExecutor.load(encoder.save_abspath)
-
-    if not requires_train_after_load:
-        # some models are not deterministic when training, so even with same training data, we cannot ensure
-        # same encoding results
-        encoded_data_test = encoder_loaded.encode(test_data)
-        np.testing.assert_array_equal(
-            encoded_data_test, encoded_data_control)
-
-
-
-def save_and_load_config(encoder, requires_train_after_load):
-    test_data = np.random.rand(10, input_dim)
-    encoder.save_config()
-    assert os.path.exists(encoder.config_abspath)
-    encoder_loaded = BaseExecutor.load_config(encoder.config_abspath)
-
-    if requires_train_after_load:
-        encoder_loaded.train(train_data)
-
-
     encoded_data_test = encoder_loaded.encode(test_data)
-    assert encoded_data_test.shape == (10, target_output_dim)
-
+    np.testing.assert_array_equal(encoded_data_test, encoded_data_control)
