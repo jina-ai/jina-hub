d85bc6eeb205c4b8bf02bb9859504845bc2f0264
David Buchaca Prats <davidbuchaca@gmail.com>
2021-04-20 12:27:18 +0200

    fix: check path to vectorizer exists (#7789)

--- encoders/nlp/TFIDFTextEncoder/__init__.py
+++ encoders/nlp/TFIDFTextEncoder/__init__.py
@@ -1,23 +1,30 @@
 __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from jina.executors.encoders import BaseEncoder
+import os
 import numpy as np
+
+from jina.executors.encoders import BaseEncoder
+from jina.excepts import PretrainedModelFileDoesNotExist
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class TFIDFTextEncoder(BaseEncoder):
     """Encode ``Document`` content from a `np.ndarray` (of strings) of length `BatchSize` into
-    a `csr_matrix` of shape `Batchsize x EmbeddingDimension`. 
+    a `csr_matrix` of shape `Batchsize x EmbeddingDimension`.
 
     :param path_vectorizer: path containing the fitted tfidf encoder object
     :param args: not used
     :param kwargs: not used
     """
 
-    def __init__(self,
-                 path_vectorizer="./model/tfidf_vectorizer.pickle",
-                 *args,
-                 **kwargs):
+    def __init__(
+        self,
+        path_vectorizer: str = os.path.join(cur_dir, 'model/tfidf_vectorizer.pickle'),
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
         self.path_vectorizer = path_vectorizer
 
@@ -27,7 +34,11 @@ class TFIDFTextEncoder(BaseEncoder):
 
         super().post_init()
         if os.path.exists(self.path_vectorizer):
-            self.tfidf_vectorizer = pickle.load(open(self.path_vectorizer, "rb"))
+            self.tfidf_vectorizer = pickle.load(open(self.path_vectorizer, 'rb'))
+        else:
+            raise PretrainedModelFileDoesNotExist(
+                f'{self.path_vectorizer} not found, cannot find a fitted tfidf_vectorizer'
+            )
 
     def encode(self, content: np.ndarray, *args, **kwargs) -> 'scipy.sparse.csr_matrix':
         """Encode the ``Document`` content creating a tf-idf feature vector of the input.
