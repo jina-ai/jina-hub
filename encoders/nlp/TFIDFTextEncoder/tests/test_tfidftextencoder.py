1e69104fd8c030014d3c96824dba1173850ccbf2
David Buchaca Prats <davidbuchaca@gmail.com>
2021-04-20 12:18:21 +0200

    fix: check path to vectorizer exists

--- encoders/nlp/TFIDFTextEncoder/tests/test_tfidftextencoder.py
+++ encoders/nlp/TFIDFTextEncoder/tests/test_tfidftextencoder.py
@@ -1,8 +1,11 @@
 import os
-import scipy
 
 import numpy as np
+import scipy
+import pytest
+
 from .. import TFIDFTextEncoder
+from jina.excepts import PretrainedModelFileDoesNotExist
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,18 +17,26 @@ def print_array_info(x, x_varname):
     print(f'{x_varname}.shape={x.shape}')
 
 
+def test_missing_path_vectorizer():
+    wrong_path_vectorizer = os.path.join(
+        cur_dir, '/model/tfidf_nonexistent_path.pickle'
+    )
+    with pytest.raises(PretrainedModelFileDoesNotExist):
+        encoder = TFIDFTextEncoder(wrong_path_vectorizer)
+
+
 def test_tfidf_text_encoder():
     # Input
     text = np.array(['Han likes eating pizza'])
 
-    # Encoder embedding 
+    # Encoder embedding
     encoder = TFIDFTextEncoder()
 
     print_array_info(text, 'text')
     embeddeding = encoder.encode(text)
     print_array_info(embeddeding, 'embeddeding')
 
-    # Compare with ouptut 
+    # Compare with ouptut
     expected = scipy.sparse.load_npz(os.path.join(cur_dir, 'expected.npz'))
     np.testing.assert_almost_equal(embeddeding.todense(), expected.todense(), decimal=4)
     assert expected.shape[0] == len(text)
@@ -50,21 +61,25 @@ def test_tfidf_unexistant_words():
 
     # Compare with ouptut
     np.testing.assert_almost_equal(embedding_1.todense(), expected_1, decimal=4)
-    np.testing.assert_almost_equal(embedding_2.todense(), expected_2.todense(), decimal=4)
+    np.testing.assert_almost_equal(
+        embedding_2.todense(), expected_2.todense(), decimal=4
+    )
 
 
 def test_tfidf_text_encoder_batch():
     # Input
     text_batch = np.array(['Han likes eating pizza', 'Han likes pizza', 'Jina rocks'])
 
-    # Encoder embedding 
+    # Encoder embedding
     encoder = TFIDFTextEncoder()
 
     print_array_info(text_batch, 'text_batch')
     embeddeding_batch = encoder.encode(text_batch)
     print_array_info(embeddeding_batch, 'embeddeding_batch')
 
-    # Compare with ouptut 
+    # Compare with ouptut
     expected_batch = scipy.sparse.load_npz(os.path.join(cur_dir, 'expected_batch.npz'))
-    np.testing.assert_almost_equal(embeddeding_batch.todense(), expected_batch.todense(), decimal=2)
+    np.testing.assert_almost_equal(
+        embeddeding_batch.todense(), expected_batch.todense(), decimal=2
+    )
     assert expected_batch.shape[0] == len(text_batch)
