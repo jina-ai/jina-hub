58184e8dfe15ddafd0f0d6401f1c44a4c9048491
Joan Fontanals Martinez <joan.martinez@jina.ai>
2021-04-20 09:22:25 +0200

    fix: fix channel axis

--- segmenters/image/TorchObjectDetectionSegmenter/__init__.py
+++ segmenters/image/TorchObjectDetectionSegmenter/__init__.py
@@ -54,7 +54,7 @@ class TorchObjectDetectionSegmenter(TorchDevice, BaseSegmenter):
         if self.model_name is None:
             self.model_name = 'fasterrcnn_resnet50_fpn'
         self.channel_axis = channel_axis
-        self._default_channel_axis = 0
+        self._default_channel_axis = 1
         self.confidence_threshold = confidence_threshold
         self.label_name_map = label_name_map
         if self.label_name_map is None:
@@ -68,7 +68,7 @@ class TorchObjectDetectionSegmenter(TorchDevice, BaseSegmenter):
         self.model = model.eval()
         self.to_device(self.model)
 
-    def _predict(self, img: 'np.ndarray') -> 'np.ndarray':
+    def _predict(self, batch: 'np.ndarray') -> 'np.ndarray':
         """
         Run the model for prediction
 
@@ -76,17 +76,17 @@ class TorchObjectDetectionSegmenter(TorchDevice, BaseSegmenter):
         :return: the boxes, scores and labels predicted
         """
         import torch
-        _input = torch.from_numpy(img.astype('float32'))
+        _input = torch.from_numpy(batch.astype('float32'))
 
         if self.on_gpu:
             _input = _input.cuda()
 
-        _predictions = []
+        batched_predictions = []
         for i in range(_input.shape[0]):
-            prediction = self.model([_input[i]])
-            _predictions.extend(prediction)
+            predictions = self.model([_input[i]])[0]
+            batched_predictions.append(predictions)
 
-        return _predictions
+        return batched_predictions
 
     @batching
     def segment(self, blob: 'np.ndarray', *args, **kwargs) -> List[Dict]:
@@ -98,25 +98,23 @@ class TorchObjectDetectionSegmenter(TorchDevice, BaseSegmenter):
         :param args:  Additional positional arguments
         :param kwargs: Additional keyword arguments
         """
-        raw_img = np.copy(blob) # (2, 681, 1264, 3) with imgs/cars.jpg
+        batch = np.copy(blob) # (2, 681, 1264, 3) with imgs/cars.jpg
         # "Ensure the color channel axis is the default axis." i.e. c comes first
-        # e.g. (h,w,c) -> (c,h,w) / (n,h,w,c) -> (c,n,h,w)
-        raw_img = _move_channel_axis(raw_img, self.channel_axis, self._default_channel_axis)
+        # e.g. (h,w,c) -> (c,h,w) / (b,h,w,c) -> (b,c,h,w)
+        batch = _move_channel_axis(batch, self.channel_axis, self._default_channel_axis)
 
-        batchedImgArr = np.asarray([raw_img[:, i, :, :] for i in range(raw_img.shape[1])])
-        batchedPredictions = self._predict(batchedImgArr)
+        batched_predictions = self._predict(batch)
 
         result = []
-        for i in range(len(batchedPredictions)):
-            pred = batchedPredictions[i]
-            bboxes = pred['boxes'].detach()
-            scores = pred['scores'].detach()
-            labels = pred['labels']
+        for i, predictions in enumerate(batched_predictions):
+            bboxes = predictions['boxes'].detach()
+            scores = predictions['scores'].detach()
+            labels = predictions['labels']
             if self.on_gpu:
                 bboxes = bboxes.cpu()
                 scores = scores.cpu()
                 labels = labels.cpu()
-            img = _load_image(raw_img[:, i, :, :] * 255, self._default_channel_axis)
+            img = _load_image(batch[i, :, :, :] * 255, self._default_channel_axis)
 
             batched = []
             for bbox, score, label in zip(bboxes.numpy(), scores.numpy(), labels.numpy()):
