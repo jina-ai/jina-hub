bf52aac065f2c73740ec42cb537fd5ad68d0c033
David Buchaca <davidbuchaca@gmail.com>
2021-04-23 23:46:43 +0200

    fix: update code with documentation

--- crafters/nlp/SpellChecker/pyngramspell/__init__.py
+++ crafters/nlp/SpellChecker/pyngramspell/__init__.py
@@ -11,6 +11,14 @@ from collections import Counter
 
 
 class BKTree:
+    """
+    :class:`BKTree` allows fast search of words up to n distances away 
+
+    :param distfn : edit distance function. 
+    :param words : iterable of words to build the tree.
+    :param sort_candidates : If true results will be sorted by distance.
+    """
+
     def __init__(self, distfn, words, sort_candidates=False):
         self.distfn = distfn
         self.sort_candidates = sort_candidates
@@ -31,7 +39,12 @@ class BKTree:
             children[d] = (word, {})
        
     def _search_descendants(self, parent, n, distance, query_word):
-        
+        """
+        :param parent : parent in the tree 
+        :param n : maximum edit distance allowed.
+        :param distance : function used to compute the edit distance.
+        :param query_word : query word
+        """
         node_word, children_dict = parent
         dist_to_node = distance(query_word, node_word)
         self.visited_nodes.append(node_word)
@@ -47,7 +60,11 @@ class BKTree:
         return results
             
     def query(self, query_word, n, return_distances=False):
-        # sort by distance
+        """
+        :param query_word : ancher word used to find candidates.
+        :param n : maximum edit distance allowed
+        :param return_distances : whether to return distances (True means return distance)
+        """
         self.visited_nodes = []
         
         distance_candidate_list = self._search_descendants(self.tree, n, self.distfn, query_word)
@@ -61,7 +78,20 @@ class BKTree:
 
 
 class PyNgramSpell():
+    """
+    :class:`PyNgramSpell` Corrects mispelled words from strings.
 
+    :param ngram_range : ngram_range (NOW THIS IS FIXED future versions will improve).
+    :param tokenizer : tokenizer function to tokenize an input string.
+    :param vocabulary : Vocabulary previosly set.
+    :param string_preprocessor_func : function used to preprocess the string, `lower` by default.
+    :param token_pattern : Regular expression used to tokenize an input string.
+    :param lambda_interpolation : Regularization assigned to the interpolation.
+    :param min_freq : Minimum frequency for a word to be  in the vocabulary.
+    :param max_dist : Maximum edit distance allowed to generate candidate tokens.
+    :param sort_candidates : Provide candidates sorted by edit distance.
+    :param bktree : True if bktree is used to search candidates. False means exhaustive search.
+    """
 
     def __init__(self,
                  ngram_range=(1,2),
@@ -88,8 +118,6 @@ class PyNgramSpell():
 
     def build_tokenizer(self):
         """Return a function that splits a string into a sequence of tokens.
-        tokenizer: callable
-              A function to split a string into a sequence of tokens.
         """
         if self.tokenizer is not None:
             return self.tokenizer
@@ -103,9 +131,9 @@ class PyNgramSpell():
 
         return token_pattern.findall
 
-    def fit(self, X):
-        """
-        X iterable of strings (corpus)        
+    def fit(self, X: Iterable[str]):
+        """Fit the ngram model and the vocabulary from the training data.    
+        :param X : Iterable over strings containing the corpus used to train the spellcheker.
         """
         self.tokenize_func = self.build_tokenizer()
         X_tokenized = [self.tokenize_func(self.string_preprocessor_func(x)) for x in X]
@@ -122,6 +150,10 @@ class PyNgramSpell():
 
 
     def get_candidates(self, token, max_dist):
+        """Get the candidate words with at most `max_dist`
+        :param token : token to be corrected.
+        :param max_dist : maximum allowed edit distance.
+        """
         if self.bktree:
             return self.get_candidates_bktree(token, max_dist)
         else:
@@ -129,6 +161,8 @@ class PyNgramSpell():
 
     def get_candidates_bktree(self, token, max_dist):
         """Return a list of candidate words from the vocabulary at most `max_dist` away from the input token.
+        :param token : token to be corrected.
+        :param max_dist : maximum allowed edit distance.
         """
         candidate_tokens = self.bktree.query(token, max_dist)
 
@@ -142,6 +176,8 @@ class PyNgramSpell():
         This version of the function is private and kept for benchmarking pourposes. This function computes the
         edit distance between the input token and all words in the vocabulary. Then it filters candidates by
         the edit distance.
+        :param token : word to be corrected.
+        :param max_dist : maximum distance allowed for candidates.
         """
         token = token.lower()
         distance_token_to_words = {word:edit_distance(word, token) for word in self.vocabulary}
@@ -158,10 +194,16 @@ class PyNgramSpell():
             return [token]
 
     def filter_vocabulary(self, min_freq):
+        """Remove words from the vocabulary that do not have more than `min_freq`
+        :param min_freq : minimum frequency required to be in the vocabulary
+        """
         self.vocabulary = set(dict(filter(lambda x:x[1]>min_freq, self.unigram_freq_dict.items())).keys())
 
 
     def correct_with_bigrams(self, tokenized_sentence):
+        """Correct the words in the tokenized_sentence that are not part of the vocabulary
+        :param tokenized_sentence : minimum frequency required to be in the vocabulary
+        """
 
         def prob_word(word):
             return self.unigram_freq_dict.get(word,0) / len(self.unigram_freq_dict)
@@ -177,8 +219,8 @@ class PyNgramSpell():
             if self.bigram_freq_dict.get(bigram, 0)  == 0:
                 return 0
             else:
-                return self.bigram_freq_dict.get(bigram, 0)/count_bigrams(bigrams_starting_by(word1))
-        
+                return self.bigram_freq_dict.get(bigram, 0)/ self.unigram_freq_dict[word1]
+
         def interpolation_probability(word1, word2): 
             n_vocabulary = len(self.vocabulary)
             return (1-self.lambda_interpolation) * probability_bigram(word1, word2, self.bigram_freq_dict) +\
@@ -199,7 +241,7 @@ class PyNgramSpell():
 
     def transform(self, x, tokenize=True):
         """Corrects the misspelled words on each element from X.
-        X iterable of strings        
+        :param x : string to be corrected      
         """
         x = self.string_preprocessor_func(x)
 
@@ -213,7 +255,10 @@ class PyNgramSpell():
         return tokenized_sentence
 
 
-    def save(self, path):
+    def save(self, file_path='./spellcheker.pickle'):
         """Save the instance an PyNgramSpell to the provided path.
+        :param file_path : path where to store the spell checker  object     
         """
-        pass
+        import pickle
+        with open(file_path, 'wb') as f:
+            pickle.dump(self, f)
