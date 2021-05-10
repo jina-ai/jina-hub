529e80c054bc7867a24cee8d82e0f2ad02e4ac5b
bwanglzu <bo.wang@jina.ai>
2021-05-10 15:55:28 +0200

    feat: add lightgbm ranker trainer

--- ranker_trainer/LightGBMRankerTrainer/__init__.py
+++ ranker_trainer/LightGBMRankerTrainer/__init__.py
@@ -1,20 +1,54 @@
+import os
+from typing import Tuple, List, Dict
+
+import lightgbm as lgb
 
 from jina.executors.rankers.trainer import RankerTrainer
+
 
 class LightGBMRankerTrainer(RankerTrainer):
     """Ranker trainer to train the `LightGBMRanker` to enable offline/online-learning."""
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(
+        self,
+        model_path: str,
+        param: Dict,
+        query_feature_names: Tuple[str],
+        match_feature_names: Tuple[str],
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.model = None
+        self.param = param
+        self.model_path = model_path
+        self._is_trained = False
+
+    def post_init(self):
+        """Load the model."""
+        if os.path.exists(self.model_path):
+            self.model = lgb.Booster(model_file=self.model_path)
+        else:
+            raise FileNotFoundError(f'The model path {self.model_path} not found.')
 
     def train(self, *args, **kwargs):
         """Train ranker based on user feedback, updating ranker weights based on
         the `loss` function.
+
         :param args: Additional arguments.
         :param kwargs: Additional key value arguments.
         """
-        pass
+        # self.model = lgb.train(self.param, train_data, 2, valid_sets=[validation_data])
+        self._is_trained = True
 
     def save(self):
         """Save the of the ranker model."""
-        pass
+        if not self.is_trained:
+            msg = 'The model has not been trained.'
+            msg += 'Will skip the save since the model is the same as the original one.'
+            raise ValueError(msg)
+        self.model.save_model(self.model_path)
+
+    @property
+    def is_trained(self):
+        return self._is_trained
