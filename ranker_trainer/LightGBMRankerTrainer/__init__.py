b5b18da7777c047e4889161b9207ca89cab21828
bwanglzu <bo.wang@jina.ai>
2021-05-19 14:31:23 +0200

    feat: fix ranker parameters and test

--- ranker_trainer/LightGBMRankerTrainer/__init__.py
+++ ranker_trainer/LightGBMRankerTrainer/__init__.py
@@ -1,67 +1,134 @@
 import os
-from typing import Dict
+from typing import Dict, List, Tuple, Optional
 
+import numpy as np
 import lightgbm as lgb
 
 from jina.executors.rankers.trainer import RankerTrainer
+from jina.excepts import PretrainedModelFileDoesNotExist
 
 
 class LightGBMRankerTrainer(RankerTrainer):
     """Ranker trainer to train the `LightGBMRanker` to enable offline/online-learning.
 
     :param model_path: Path to the pretrained model previously trained using LightGBM.
-    :param param: Parameters for training.
-    :param train_set: Data to be trained on.
     """
 
     def __init__(
         self,
         model_path: str,
-        train_set: lgb.Dataset,
-        valid_set: lgb.Dataset,
-        param: Dict,
-        verbose_eval: int = 10,
+        params: Dict,
+        query_feature_names: Tuple[str],
+        match_feature_names: Tuple[str],
+        query_groups: List[int],
+        match_groups: List[int],
+        query_categorical_features: Optional[List[str]] = None,
+        match_categorical_features: Optional[List[str]] = None,
+        query_features_before: bool = True,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.model = None
-        self.param = param
-        self.train_set = train_set
-        self.valid_set = valid_set
+        self.params = params
         self.model_path = model_path
-        self.verbose_eval = verbose_eval
+        self.query_feature_names = query_feature_names
+        self.match_feature_names = match_feature_names
+        self.query_groups = query_groups
+        self.match_groups = match_groups
+        self.query_categorical_features = query_categorical_features
+        self.match_categorical_features = match_categorical_features
+        self.query_features_before = query_features_before
 
     def post_init(self):
         """Load the model."""
         super().post_init()
-        if os.path.exists(self.model_path):
+        if self.model_path and os.path.exists(self.model_path):
             self.model = lgb.Booster(model_file=self.model_path)
+            model_num_features = self.model.num_feature()
+            expected_num_features = len(
+                self.query_feature_names + self.match_feature_names
+            )
+            if model_num_features != expected_num_features:
+                raise ValueError(
+                    f'The number of features expected by the LightGBM model {model_num_features} is different'
+                    f'than the ones provided in input {expected_num_features}'
+                )
         else:
-            msg = f'The model path {self.model_path} not found.'
-            msg += 'Will train from scratch once you call `train` method!'
-            self.logger.info(msg)
+            raise PretrainedModelFileDoesNotExist(
+                f'model {self.model_path} does not exist'
+            )
 
-    def train(self, *args, **kwargs):
+    def _get_features_dataset(self, query_meta, match_meta):
+        def _get_features_per_query(q_meta, m_meta):
+            query_features = np.array(
+                [
+                    [q_meta[feat] for feat in self.query_feature_names]
+                    for _ in range(0, len(m_meta))
+                ]
+            )
+            match_features = np.array(
+                [[meta[feat] for feat in self.match_feature_names] for meta in m_meta]
+            )
+            return query_features, match_features
+
+        q_features, m_features = [], []
+        for q_meta, m_meta in zip(query_meta, match_meta):
+            q_f, m_f = _get_features_per_query(q_meta, m_meta)
+            q_features.append(q_f)
+            m_features.append(m_f)
+
+        query_features = np.vstack(q_features)
+        query_dataset = lgb.Dataset(
+            data=query_features,
+            group=self.query_groups,
+            feature_name=self.query_feature_names,
+            categorical_feature=self.query_categorical_features,
+            free_raw_data=False,
+        )
+        match_features = np.vstack(m_features)
+        match_dataset = lgb.Dataset(
+            data=match_features,
+            group=self.match_groups,
+            feature_name=self.match_feature_names,
+            categorical_feature=self.match_categorical_features,
+            free_raw_data=False,
+        )
+        if self.query_features_before:
+            return query_dataset.construct().add_features_from(
+                match_dataset.construct()
+            )
+        else:
+            return match_dataset.construct().add_features_from(
+                query_dataset.construct()
+            )
+
+    def train(
+        self,
+        query_metas: List[Dict],
+        matches_metas: List[List[Dict]],
+        *args,
+        **kwargs,
+    ):
         """Train ranker based on user feedback, updating ranker in an incremental fashion.
 
         This function make use of lightgbm `train` to update the tree structure through continued
         training.
 
+        :param query_metas: List of features extracted from query `Document`. Extracted according to `query_feature_names`.
+            The length of the list is the number of features for which the scores will be computed
+        :param matches_metas: List of list of features extracted from match `Document`. Extracted according to `match_feature_names`.
+            The list's length is equal to the number of queries, and each element is a list of the number of matches.
         :param args: Additional arguments.
         :param kwargs: Additional key value arguments.
         """
-        eval_res = {}
+        train_set = self._get_features_dataset(query_metas, matches_metas)
         self.model = lgb.train(
-            train_set=self.train_set,
-            valid_sets=[self.valid_set],
+            train_set=train_set,
             init_model=self.model,
-            evals_result=eval_res,
-            verbose_eval=self.verbose_eval,
-            params=self.param,
+            params=self.params,
             keep_training_booster=True,
         )
-        return eval_res
 
     def save(self):
         """Save the trained lightgbm ranker model."""
--- ranker_trainer/LightGBMRankerTrainer/tests/test_lightgbm_ranker_trainer.py
+++ ranker_trainer/LightGBMRankerTrainer/tests/test_lightgbm_ranker_trainer.py
@@ -9,23 +9,66 @@ from .. import LightGBMRankerTrainer
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 
+query_features = ['tags__query_length', 'tags__query_language']
+match_features = [
+    'tags__document_length',
+    'tags__document_language',
+    'tags__document_pagerank',
+]
+
+
+def _pretrained_model(model_path):
+    from sklearn.datasets import load_svmlight_file
+    import lightgbm as lgb
+
+    X_train, y_train = load_svmlight_file(
+        os.path.join(cur_dir, 'training_dataset.train')
+    )
+    X_train = X_train.todense()
+    # randomize pagerank feature so that there is something to be trained
+    X_train[:, -1] = np.random.randint(2, size=(25, 1))
+
+    q_train = np.loadtxt(os.path.join(cur_dir, 'training_dataset.train.query'))
+    lgb_train = lgb.Dataset(
+        np.asarray(X_train),
+        y_train,
+        group=q_train,
+        free_raw_data=False,
+        feature_name=query_features + match_features,
+        params={
+            'min_data_in_bin': 1,
+            'verbose': 1,
+            'max_bin': 2,
+            'min_data_in_leaf': 1,
+        },
+    )
+
+    param = {
+        'num_leaves': 2,
+        'objective': 'lambdarank',
+        'metric': 'ndcg',
+        'min_data_in_bin': 1,
+        'max_bin': 2,
+        'num_trees': 1,
+        'learning_rate': 0.05,
+        'min_data_in_leaf': 1,
+    }
+    booster = lgb.train(param, lgb_train, 2, valid_sets=[lgb_train])
+    booster.save_model(model_path)
+    return model_path
+
 
 @pytest.fixture
-def train_data():
-    data = lgb.Dataset(os.path.join(cur_dir, 'rank.train'))
-    group_data = [
-        l.strip("\n") for l in open(os.path.join(cur_dir, 'rank.train.query'))
-    ]
-    data.set_group(group_data)
-    return data
-
-
-@pytest.fixture
-def valid_data():
-    data = lgb.Dataset(os.path.join(cur_dir, 'rank.test'))
-    group_data = [l.strip("\n") for l in open(os.path.join(cur_dir, 'rank.test.query'))]
-    data.set_group(group_data)
-    return data
+def model(tmpdir):
+    """
+    Pretrained model to be stored in tmpdir.
+    It will be trained on fake data forcing that the first feature correlates positively with relevance, while the rest are
+    are random
+    :param tmpdir:
+    :return:
+    """
+    model_path = os.path.join(tmpdir, 'model.txt')
+    return _pretrained_model(model_path)
 
 
 @pytest.fixture
@@ -50,25 +93,72 @@ def param():
     }
 
 
-def get_ndcg_eval_res_at_k(k, eval_res, valid_set):
-    return eval_res[valid_set]['ndcg@' + str(k)]
+@pytest.fixture
+def query_metas():
+    return [
+        {
+            'tags__query_length': 1.0,
+            'tags__query_language': 0.0,
+        },
+        {
+            'tags__query_length': 1.0,
+            'tags__query_language': 0.0,
+        },
+    ]
+
+
+@pytest.fixture
+def matches_metas():
+    return [
+        [
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 0.0,
+            },
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 1.0,
+            },
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 0.0,
+            },
+        ],
+        [
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 1.0,
+            },
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 0.0,
+            },
+            {
+                'tags__document_length': 0.0,
+                'tags__document_language': 0.0,
+                'tags__document_pagerank': 1.0,
+            },
+        ],
+    ]
 
 
 def test_ranker_trainer(
-    train_data,
-    valid_data,
     param,
+    model,
+    query_metas,
+    matches_metas,
 ):
     ranker_trainer = LightGBMRankerTrainer(
-        model_path='test.txt',
-        train_set=train_data,
-        valid_set=valid_data,
-        param=param,
+        model_path=model,
+        params=param,
+        query_feature_names=query_features,
+        match_feature_names=match_features,
+        query_groups=[3, 3],
+        match_groups=[3, 3],
     )
-    eval_res = ranker_trainer.train()
-    # mearesure ndcg 100 times @ different levels
-    # assert ndcg@k increases after training
-    valid_zero_at_5 = get_ndcg_eval_res_at_k(5, eval_res, 'valid_0')
-    assert valid_zero_at_5[0] <= valid_zero_at_5[50] <= valid_zero_at_5[99]
-    valid_zero_at_10 = get_ndcg_eval_res_at_k(10, eval_res, 'valid_0')
-    assert valid_zero_at_10[0] <= valid_zero_at_10[50] <= valid_zero_at_10[99]
+    ranker_trainer.train(query_metas, matches_metas)
